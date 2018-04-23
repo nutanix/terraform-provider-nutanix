e4eecde0115788d31d6c70ea489a6ca585da9947
Marin Salinas <marinssalinas@gmail.com>
2018-04-23 11:44:46 -0500

    Add Subnet Operations to make /subnets API requests

--- client/v3/v3_service.go
+++ client/v3/v3_service.go
@@ -20,6 +20,11 @@ type Service interface {
 	GetVM(UUID string) (*VMIntentResponse, error)
 	ListVM(getEntitiesRequest VMListMetadata) (*VMListIntentResponse, error)
 	UpdateVM(UUID string, body VMIntentInput) (*VMIntentResponse, error)
+	CreateSubnet(createRequest SubnetIntentInput) (*SubnetIntentResponse, error)
+	DeleteSubnet(UUID string) error
+	GetSubnet(UUID string) (*SubnetIntentResponse, error)
+	ListSubnet(getEntitiesRequest SubnetListMetadata) (*SubnetListIntentResponse, error)
+	UpdateSubnet(UUID string, body SubnetIntentInput) (*SubnetIntentResponse, error)
 }
 
 /*CreateVM Creates a VM
@@ -140,4 +145,127 @@ func (vm Operations) UpdateVM(UUID string, body VMIntentInput) (*VMIntentRespons
 	}
 
 	return vmIntentResponse, nil
+}
+
+/*CreateSubnet Creates a subnet
+ * This operation submits a request to create a subnet based on the input parameters. A subnet is a block of IP addresses.
+ *
+ * @param body
+ * @return *SubnetIntentResponse
+ */
+func (vm Operations) CreateSubnet(createRequest SubnetIntentInput) (*SubnetIntentResponse, error) {
+	ctx := context.TODO()
+
+	req, err := vm.client.NewRequest(ctx, http.MethodPost, "/subnets", createRequest)
+	if err != nil {
+		return nil, err
+	}
+
+	subnetIntentResponse := new(SubnetIntentResponse)
+
+	err = vm.client.Do(ctx, req, subnetIntentResponse)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return subnetIntentResponse, nil
+}
+
+/*DeleteSubnet Deletes a subnet
+ * This operation submits a request to delete a subnet.
+ *
+ * @param uuid The UUID of the entity.
+ * @return error if exist error
+ */
+func (vm Operations) DeleteSubnet(UUID string) error {
+	ctx := context.TODO()
+
+	path := fmt.Sprintf("/subnets/%s", UUID)
+
+	req, err := vm.client.NewRequest(ctx, http.MethodDelete, path, nil)
+	if err != nil {
+		return err
+	}
+
+	return vm.client.Do(ctx, req, nil)
+}
+
+/*GetSubnet Gets a subnet entity
+ * This operation gets a subnet.
+ *
+ * @param uuid The UUID of the entity.
+ * @return *SubnetIntentResponse
+ */
+func (vm Operations) GetSubnet(UUID string) (*SubnetIntentResponse, error) {
+	ctx := context.TODO()
+
+	path := fmt.Sprintf("/subnets/%s", UUID)
+
+	req, err := vm.client.NewRequest(ctx, http.MethodGet, path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	subnetIntentResponse := new(SubnetIntentResponse)
+
+	err = vm.client.Do(ctx, req, subnetIntentResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return subnetIntentResponse, nil
+}
+
+/*ListSubnet Gets a list of subnets
+ * This operation gets a list of subnets, allowing for sorting and pagination. Note: Entities that have not been created successfully are not listed.
+ *
+ * @param getEntitiesRequest
+ * @return *SubnetListIntentResponse
+ */
+func (vm Operations) ListSubnet(getEntitiesRequest SubnetListMetadata) (*SubnetListIntentResponse, error) {
+	ctx := context.TODO()
+	path := "/subnets/list"
+
+	req, err := vm.client.NewRequest(ctx, http.MethodPost, path, getEntitiesRequest)
+
+	if err != nil {
+		return nil, err
+	}
+
+	subnetListIntentResponse := new(SubnetListIntentResponse)
+	err = vm.client.Do(ctx, req, subnetListIntentResponse)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return subnetListIntentResponse, nil
+}
+
+/*UpdateSubnet Updates a subnet
+ * This operation submits a request to update a subnet based on the input parameters.
+ *
+ * @param uuid The UUID of the entity.
+ * @param body
+ * @return *SubnetIntentResponse
+ */
+func (vm Operations) UpdateSubnet(UUID string, body SubnetIntentInput) (*SubnetIntentResponse, error) {
+	ctx := context.TODO()
+
+	path := fmt.Sprintf("/subnets/%s", UUID)
+
+	req, err := vm.client.NewRequest(ctx, http.MethodPut, path, body)
+	if err != nil {
+		return nil, err
+	}
+
+	subnetIntentResponse := new(SubnetIntentResponse)
+
+	err = vm.client.Do(ctx, req, subnetIntentResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return subnetIntentResponse, nil
 }
