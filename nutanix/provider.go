a439b28663900e4acae40e9b8eed82b9d05a563c
Marin Salinas <marinssalinas@gmail.com>
2018-05-01 16:44:16 -0500

    expose subnet data source in provider

--- nutanix/provider.go
+++ nutanix/provider.go
@@ -46,7 +46,7 @@ func Provider() terraform.ResourceProvider {
 		DataSourcesMap: map[string]*schema.Resource{
 			"nutanix_virtual_machine": dataSourceNutanixVirtualMachine(),
 			// "nutanix_image":  dataSourceNutanixImage(),
-			// "nutanix_subnet": dataSourceNutanixSubnet(),
+			"nutanix_subnet": dataSourceNutanixSubnet(),
 		},
 		ResourcesMap: map[string]*schema.Resource{
 			"nutanix_virtual_machine": resourceNutanixVirtualMachine(),
