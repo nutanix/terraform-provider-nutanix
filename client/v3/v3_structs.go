1b6aee647228d4ffa5721a44eb7d166adfecdea8
Marin Salinas <marinssalinas@gmail.com>
2018-05-04 11:55:40 -0500

    add strutcs for categories requests

--- client/v3/v3_structs.go
+++ client/v3/v3_structs.go
@@ -1389,3 +1389,214 @@ type Credentials struct {
 type ClusterAnalysis struct {
 	VMEfficiencyMap map[string]interface{} `json:"vm_efficiency_map,omitempty"`
 }
+
+//CategoryListMetadata All api calls that return a list will have this metadata block as input
+type CategoryListMetadata struct {
+
+	// The filter in FIQL syntax used for the results.
+	Filter *string `json:"filter,omitempty"`
+
+	// The kind name
+	Kind *string `json:"kind,omitempty"`
+
+	// The number of records to retrieve relative to the offset
+	Length *int64 `json:"length,omitempty"`
+
+	// Offset from the start of the entity list
+	Offset *int64 `json:"offset,omitempty"`
+
+	// The attribute to perform sort on
+	SortAttribute *string `json:"sort_attribute,omitempty"`
+
+	// The sort order in which results are returned
+	SortOrder *string `json:"sort_order,omitempty"`
+}
+
+//CategoryKeyStatus represents Category Key Definition.
+type CategoryKeyStatus struct {
+
+	// API version.
+	APIVersion *string `json:"api_version,omitempty"`
+
+	// Description of the category.
+	Description *string `json:"description,omitempty"`
+
+	// Name of the category.
+	Name *string `json:"name"`
+
+	// Specifying whether its a system defined category.
+	SystemDefined *bool `json:"system_defined,omitempty"`
+}
+
+//CategoryKeyListResponse represents the category key list response.
+type CategoryKeyListResponse struct {
+
+	// API Version.
+	APIVersion *string `json:"api_version,omitempty"`
+
+	Entities []*CategoryKeyStatus `json:"entities,omitempty"`
+
+	Metadata *CategoryListMetadata `json:"metadata,omitempty"`
+}
+
+//CategoryKey represents category key definition.
+type CategoryKey struct {
+
+	// API version.
+	APIVersion *string `json:"api_version,omitempty"`
+
+	// Description of the category.
+	Description *string `json:"description,omitempty"`
+
+	// Name of the category.
+	Name *string `json:"name"`
+}
+
+//CategoryStatus represents The status of a REST API call. Only used when there is a failure to report.
+type CategoryStatus struct {
+	APIVersion *string `json:"api_version,omitempty"`
+
+	// The HTTP error code.
+	Code *int64 `json:"code,omitempty"`
+
+	// The kind name
+	Kind *string `json:"kind,omitempty"`
+
+	MessageList []*MessageResource `json:"message_list,omitempty"`
+
+	State *string `json:"state,omitempty"`
+}
+
+//CategoryValueListResponse represents Category Value list response.
+type CategoryValueListResponse struct {
+	APIVersion *string `json:"api_version,omitempty"`
+
+	Entities []*CategoryValueStatus `json:"entities,omitempty"`
+
+	Metadata *CategoryListMetadata `json:"metadata,omitempty"`
+}
+
+//CategoryValueStatus represents Category value definition.
+type CategoryValueStatus struct {
+
+	// API version.
+	APIVersion *string `json:"api_version,omitempty"`
+
+	// Description of the category value.
+	Description *string `json:"description,omitempty"`
+
+	// The name of the category.
+	Name *string `json:"name,omitempty"`
+
+	// Specifying whether its a system defined category.
+	SystemDefined *bool `json:"system_defined,omitempty"`
+
+	// The value of the category.
+	Value *string `json:"value,omitempty"`
+}
+
+//CategoryFilter represents A category filter.
+type CategoryFilter struct {
+
+	// List of kinds associated with this filter.
+	KindList []*string `json:"kind_list,omitempty"`
+
+	// A list of category key and list of values.
+	Params map[string][]*string `json:"params"`
+
+	// The type of the filter being used.
+	Type *string `json:"type,omitempty"`
+}
+
+//CategoryQueryInput represents Categories query input object.
+type CategoryQueryInput struct {
+
+	// API version.
+	APIVersion *string `json:"api_version,omitempty"`
+
+	CategoryFilter *CategoryFilter `json:"category_filter,omitempty"`
+
+	// The maximum number of members to return per group.
+	GroupMemberCount *int64 `json:"group_member_count,omitempty"`
+
+	// The offset into the total member set to return per group.
+	GroupMemberOffset *int64 `json:"group_member_offset,omitempty"`
+
+	//TBD: USED_IN - to get policies in which specified categories are used. APPLIED_TO - to get entities attached to specified categories.
+	UsageType *string `json:"usage_type,omitempty"`
+}
+
+//CategoryQueryResponseMetadata represents Response metadata.
+type CategoryQueryResponseMetadata struct {
+
+	// The maximum number of records to return per group.
+	GroupMemberCount *int64 `json:"group_member_count,omitempty"`
+
+	// The offset into the total records set to return per group.
+	GroupMemberOffset *int64 `json:"group_member_offset,omitempty"`
+
+	// Total number of matched results.
+	TotalMatches *int64 `json:"total_matches,omitempty"`
+
+	//TBD: USED_IN - to get policies in which specified categories are used. APPLIED_TO - to get entities attached to specified categories.
+	UsageType *string `json:"usage_type,omitempty"`
+}
+
+//EntityReference Reference to an entity.
+type EntityReference struct {
+
+	// Categories for the entity.
+	Categories map[string]string `json:"categories,omitempty"`
+
+	// Kind of the reference.
+	Kind *string `json:"kind,omitempty"`
+
+	// Name of the entity.
+	Name *string `json:"name,omitempty"`
+
+	// The type of filter being used. (Options : CATEGORIES_MATCH_ALL , CATEGORIES_MATCH_ANY)
+	Type *string `json:"type,omitempty"`
+
+	// UUID of the entity.
+	UUID *string `json:"uuid,omitempty"`
+}
+
+//CategoryQueryResponseResults ...
+type CategoryQueryResponseResults struct {
+
+	// List of entity references.
+	EntityAnyReferenceList []*EntityReference `json:"entity_any_reference_list,omitempty"`
+
+	// Total number of filtered results.
+	FilteredEntityCount *int64 `json:"filtered_entity_count,omitempty"`
+
+	// The entity kind.
+	Kind *string `json:"kind,omitempty"`
+
+	// Total number of the matched results.
+	TotalEntityCount *int64 `json:"total_entity_count,omitempty"`
+}
+
+//CategoryQueryResponse represents Categories query response object.
+type CategoryQueryResponse struct {
+
+	// API version.
+	APIVersion *string `json:"api_version,omitempty"`
+
+	Metadata *CategoryQueryResponseMetadata `json:"metadata,omitempty"`
+
+	Results []*CategoryQueryResponseResults `json:"results,omitempty"`
+}
+
+//CategoryValue represents Category value definition.
+type CategoryValue struct {
+
+	// API version.
+	APIVersion *string `json:"api_version,omitempty"`
+
+	// Description of the category value.
+	Description *string `json:"description,omitempty" `
+
+	// Value for the category.
+	Value *string `json:"value,omitempty"`
+}
