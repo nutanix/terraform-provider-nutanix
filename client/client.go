eba6e565b57205469f00b4f34e45d8a29230b1c9
Marin Salinas <marinssalinas@gmail.com>
2018-05-23 12:09:38 -0500

    change tests

--- client/client.go
+++ client/client.go
@@ -171,9 +171,9 @@ func (c *Client) Do(ctx context.Context, req *http.Request, v interface{}) error
 
 //CheckResponse checks errors if exist errors in request
 func CheckResponse(r *http.Response) error {
-	// if c := r.StatusCode; c >= 200 && c <= 299 {
-	// 	return nil
-	// }
+	if c := r.StatusCode; c >= 200 && c <= 299 && r.Request.Method == http.MethodDelete {
+		return nil
+	}
 
 	buf, err := ioutil.ReadAll(r.Body)
 
