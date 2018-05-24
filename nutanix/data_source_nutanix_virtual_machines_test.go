a29963d8213c69f1b3062b95df759d9d56afa2f4
Cristian Ramirez <cristiano.rosetti@gmail.com>
2018-05-24 13:42:27 -0500

    removing innecesary code

--- nutanix/data_source_nutanix_virtual_machines_test.go
+++ nutanix/data_source_nutanix_virtual_machines_test.go
@@ -53,17 +53,6 @@ resource "nutanix_virtual_machine" "vm1" {
   memory_size_mib      = 2048
   power_state          = "ON"
 
-  nic_list = [{
-    subnet_reference = {
-      kind = "subnet"
-      uuid = "${nutanix_subnet.test.id}"
-    }
-
-    ip_endpoint_list = {
-			ip = "192.168.0.10"
-			type = "ASSIGNED"
-    }
-  }]
 }
 
 resource "nutanix_virtual_machine" "vm2" {
@@ -79,42 +68,6 @@ resource "nutanix_virtual_machine" "vm2" {
   memory_size_mib      = 2048
   power_state          = "ON"
 
-  nic_list = [{
-    subnet_reference = {
-      kind = "subnet"
-      uuid = "${nutanix_subnet.test.id}"
-    }
-
-    ip_endpoint_list = {
-			ip = "192.168.0.11"
-			type = "ASSIGNED"
-    }
-  }]
-}
-
-resource "nutanix_subnet" "test" {
-  name        = "dou_vlan0_test"
-
-  cluster_reference = {
-	  kind = "cluster"
-	  uuid = "${data.nutanix_clusters.clusters.entities.0.metadata.uuid}"
-  }
-
-  vlan_id     = %d
-  subnet_type = "VLAN"
-
-  prefix_length      = 24
-  default_gateway_ip = "192.168.0.1"
-  subnet_ip          = "192.168.0.0"
-
-  dhcp_options {
-    boot_file_name   = "bootfile"
-    tftp_server_name = "192.168.0.252"
-    domain_name      = "nutanix"
-  }
-
-  dhcp_domain_name_server_list = ["8.8.8.8", "4.2.2.2"]
-  dhcp_domain_search_list      = ["nutanix.com", "calm.io"]
 }
 
 data "nutanix_virtual_machines" "basic_web" {
