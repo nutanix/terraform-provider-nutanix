459063e29bac3e7fae724705d7021beb56569d22
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2018-09-20 15:56:57 -0500

    Change categories to map on subnet resource and add corresponding test
    Description has some weird bug on the API, it does not return it
    on the get requests, omitting it on the tests

--- nutanix/resource_nutanix_subnet_test.go
+++ nutanix/resource_nutanix_subnet_test.go
@@ -45,6 +45,40 @@ func TestAccNutanixSubnet_basic(t *testing.T) {
 	})
 }
 
+func TestAccNutanixSubnet_WithCategory(t *testing.T) {
+	r := acctest.RandIntRange(3500, 3900)
+	resourceName := "nutanix_subnet.acctest-managed"
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckNutanixSubnetDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNutanixSubnetConfig_WithCategory(r),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNutanixSubnetExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "categories.%", "1"),
+					resource.TestCheckResourceAttr(resourceName, "categories.environment-terraform", "production"),
+				),
+			},
+			{
+				Config: testAccNutanixSubnetConfig_WithCategoryUpdate(r),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNutanixSubnetExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "categories.%", "1"),
+					resource.TestCheckResourceAttr(resourceName, "categories.environment-terraform", "staging"),
+				),
+			},
+			{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"description"},
+			},
+		},
+	})
+}
+
 func testAccCheckNutanixSubnetExists(n string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[n]
@@ -145,9 +179,89 @@ resource "nutanix_subnet" "acctest-managed" {
 		domain_name      = "nutanix"
 		tftp_server_name = "10.250.140.200"
 	}
-	
+
 	dhcp_domain_name_server_list = ["8.8.8.8", "4.2.2.2"]
 	dhcp_domain_search_list      = ["terraform.nutanix.com", "terraform.unit.test.com"]
+}
+`, r)
+}
+
+func testAccNutanixSubnetConfig_WithCategory(r int) string {
+	return fmt.Sprintf(`
+data "nutanix_clusters" "clusters" {}
+
+output "cluster" {
+  value = "${data.nutanix_clusters.clusters.entities.0.metadata.uuid}"
+}
+
+resource "nutanix_subnet" "acctest-managed" {
+  # What cluster will this VLAN live on?
+  cluster_reference = {
+	kind = "cluster"
+	uuid = "${data.nutanix_clusters.clusters.entities.0.metadata.uuid}"
+  }
+
+  # General Information for subnet
+	name        = "acctest-managed"
+  vlan_id     = %d
+	subnet_type = "VLAN"
+
+  # Provision a Managed L3 Network
+  # This bit is only needed if you intend to turn on AHV's IPAM
+	subnet_ip          = "10.250.140.0"
+  default_gateway_ip = "10.250.140.1"
+  prefix_length = 24
+  dhcp_options {
+		boot_file_name   = "bootfile"
+		domain_name      = "nutanix"
+		tftp_server_name = "10.250.140.200"
+	}
+	dhcp_domain_name_server_list = ["8.8.8.8", "4.2.2.2"]
+	dhcp_domain_search_list      = ["terraform.nutanix.com", "terraform.unit.test.com"]
+
+	categories {
+		environment-terraform = "production"
+	}
+}
+`, r)
+}
+
+func testAccNutanixSubnetConfig_WithCategoryUpdate(r int) string {
+	return fmt.Sprintf(`
+data "nutanix_clusters" "clusters" {}
+
+output "cluster" {
+  value = "${data.nutanix_clusters.clusters.entities.0.metadata.uuid}"
+}
+
+resource "nutanix_subnet" "acctest-managed" {
+  # What cluster will this VLAN live on?
+  cluster_reference = {
+	kind = "cluster"
+	uuid = "${data.nutanix_clusters.clusters.entities.0.metadata.uuid}"
+  }
+
+  # General Information for subnet
+	name        = "acctest-managed"
+  vlan_id     = %d
+	subnet_type = "VLAN"
+
+  # Provision a Managed L3 Network
+  # This bit is only needed if you intend to turn on AHV's IPAM
+	subnet_ip          = "10.250.140.0"
+  default_gateway_ip = "10.250.140.1"
+  prefix_length = 24
+  dhcp_options {
+		boot_file_name   = "bootfile"
+		domain_name      = "nutanix"
+		tftp_server_name = "10.250.140.200"
+	}
+	dhcp_domain_name_server_list = ["8.8.8.8", "4.2.2.2"]
+	dhcp_domain_search_list      = ["terraform.nutanix.com", "terraform.unit.test.com"]
+
+	categories {
+		environment-terraform = "staging"
+	}
 }
 `, r)
 }
