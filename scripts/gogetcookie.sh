5cf5ed27bdce49291f944dbeb7545cc8c012b009
Marin Salinas <marinssalinas@gmail.com>
2019-07-11 16:37:09 -0500

    chore: update terraform 0.12 dependencies

--- scripts/gogetcookie.sh
+++ scripts/gogetcookie.sh
@@ -7,7 +7,7 @@ eval 'set +o history' 2>/dev/null || setopt HIST_IGNORE_SPACE 2>/dev/null
  git config --global http.cookiefile ~/.gitcookies
 
  tr , \\t <<\__END__ >>~/.gitcookies
-go.googlesource.com,FALSE,/,TRUE,2147483647,o,git-marinssalinas.gmail.com=1/w0JstWTYy5pfuH1Z-XnvZRNGSKGUmf5LgukEvvZkzlu1x8WZepIFsOYlEdY2D0F_
-go-review.googlesource.com,FALSE,/,TRUE,2147483647,o,git-marinssalinas.gmail.com=1/w0JstWTYy5pfuH1Z-XnvZRNGSKGUmf5LgukEvvZkzlu1x8WZepIFsOYlEdY2D0F_
+go.googlesource.com,FALSE,/,TRUE,2147483647,o,git-marinssalinas.gmail.com=1/fzxxUwt0Hs8AFhJUiZ-IzI2fe_w6azG7h_D7X3J3kH4
+go-review.googlesource.com,FALSE,/,TRUE,2147483647,o,git-marinssalinas.gmail.com=1/fzxxUwt0Hs8AFhJUiZ-IzI2fe_w6azG7h_D7X3J3kH4
 __END__
 eval 'set -o history' 2>/dev/null || unsetopt HIST_IGNORE_SPACE 2>/dev/null
