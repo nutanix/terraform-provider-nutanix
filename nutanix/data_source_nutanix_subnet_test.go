f88ffc6289fc882ee0149235ea7c6ccc630006eb
Marin Salinas <marinssalinas@gmail.com>
2019-07-14 01:37:49 -0500

    test: change subnet datasource acc-test configuration to support tf 0.12 syntax

--- nutanix/data_source_nutanix_subnet_test.go
+++ nutanix/data_source_nutanix_subnet_test.go
@@ -51,7 +51,7 @@ func TestAccNutanixSubnetDataSource_conflicts(t *testing.T) {
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config:      testAccSubnetDataSourceConfigNameDuplicated(randIntBetween(21, 30)),
+				Config:      testAccSubnetDataSourceBadConfig(),
 				ExpectError: regexp.MustCompile("conflicts with"),
 			},
 		},
@@ -63,13 +63,15 @@ func testAccSubnetDataSourceConfig(r int) string {
 data "nutanix_clusters" "clusters" {}
 
 locals {
-	cluster1 = "${data.nutanix_clusters.clusters.entities.0.service_list.0 == "PRISM_CENTRAL"
-	? data.nutanix_clusters.clusters.entities.1.metadata.uuid : data.nutanix_clusters.clusters.entities.0.metadata.uuid}"
+	cluster1 = [
+	for cluster in data.nutanix_clusters.clusters.entities :
+	cluster.metadata.uuid if cluster.service_list[0] != "PRISM_CENTRAL"
+	][0]
 }
 
 resource "nutanix_subnet" "test" {
 	name = "dou_vlan0_test_%d"
-	cluster_uuid = "${local.cluster1}"
+	cluster_uuid = local.cluster1
 
 	vlan_id = %d
 	subnet_type = "VLAN"
@@ -90,7 +92,7 @@ resource "nutanix_subnet" "test" {
 }
 
 data "nutanix_subnet" "test" {
-	subnet_id = "${nutanix_subnet.test.id}"
+	subnet_id = nutanix_subnet.test.id
 }
 `, r, r)
 }
@@ -100,13 +102,15 @@ func testAccSubnetDataSourceConfigName(r int) string {
 data "nutanix_clusters" "clusters" {}
 
 locals {
-	cluster1 = "${data.nutanix_clusters.clusters.entities.0.service_list.0 == "PRISM_CENTRAL"
-	? data.nutanix_clusters.clusters.entities.1.metadata.uuid : data.nutanix_clusters.clusters.entities.0.metadata.uuid}"
+	cluster1 = [
+	for cluster in data.nutanix_clusters.clusters.entities :
+	cluster.metadata.uuid if cluster.service_list[0] != "PRISM_CENTRAL"
+	][0]
 }
 
 resource "nutanix_subnet" "test" {
 	name = "dou_vlan0_test_%d"
-	cluster_uuid = "${local.cluster1}"
+	cluster_uuid = local.cluster1
 	vlan_id = %d
 	subnet_type = "VLAN"
 
@@ -126,63 +130,16 @@ resource "nutanix_subnet" "test" {
 }
 
 data "nutanix_subnet" "test" {
-	subnet_name = "${nutanix_subnet.test.name}"
+	subnet_name = nutanix_subnet.test.name
 }
 `, r, r)
 }
 
-func testAccSubnetDataSourceConfigNameDuplicated(r int) string {
-	return fmt.Sprintf(`
-data "nutanix_clusters" "clusters" {}
-
-locals {
-	cluster1 = data.nutanix_clusters.clusters.entities[0].service_list[0] == "PRISM_CENTRAL" ? data.nutanix_clusters.clusters.entities[1].metadata.uuid : data.nutanix_clusters.clusters.entities[0].metadata.uuid
-}
-
-resource "nutanix_subnet" "test" {
-	name = "dou_vlan0_test_%d"
-	cluster_uuid = "local.cluster1"
-	vlan_id = %d
-	subnet_type = "VLAN"
-
-	prefix_length = 24
-	default_gateway_ip = "192.168.0.1"
-	subnet_ip = "192.168.0.0"
-	ip_config_pool_list_ranges = ["192.168.0.10 192.168.0.100"]
-
-	dhcp_options = {
-		boot_file_name   = "bootfile"
-		domain_name      = "nutanix"
-		tftp_server_name = "10.250.140.200"
-	}
-	
-	dhcp_domain_name_server_list = ["8.8.8.8", "4.2.2.2"]
-	dhcp_domain_search_list      = ["terraform.nutanix.com", "terraform.unit.test.com"]
-}
-
-resource "nutanix_subnet" "test1" {
-	name = "${nutanix_subnet.test.name}"
-	cluster_uuid= local.cluster1
-	vlan_id = %d
-	subnet_type = "VLAN"
-	prefix_length = 24
-	default_gateway_ip = "192.168.0.1"
-	subnet_ip = "192.168.0.0"
-	#ip_config_pool_list_ranges = ["192.168.0.5", "192.168.0.100"]
-
-	dhcp_options = {
-		boot_file_name   = "bootfile"
-		domain_name      = "nutanix"
-		tftp_server_name = "10.250.140.200"
-	}
-	
-	dhcp_domain_name_server_list = ["8.8.8.8", "4.2.2.2"]
-	dhcp_domain_search_list      = ["terraform.nutanix.com", "terraform.unit.test.com"]
-}
-
+func testAccSubnetDataSourceBadConfig() string {
+	return `
 data "nutanix_subnet" "test" {
-	subnet_id   = "nutanix_subnet.test1.id"
-	subnet_name = "nutanix_subnet.test1.name"
+	subnet_id   = "test-subnet-id"
+	subnet_name = "test-subnet-name"
 }
-`, r, r, r+2)
+`
 }
