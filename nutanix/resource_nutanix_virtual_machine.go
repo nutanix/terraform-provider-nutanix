9c369daa6e4de7593916170a400db10770bfb492
Yannick Struyf <yannick.struyf@nutanix.com>
2020-04-03 20:39:45 +0200

    modified CPU logic to support modification of number of vcpus per socket

--- nutanix/resource_nutanix_virtual_machine.go
+++ nutanix/resource_nutanix_virtual_machine.go
@@ -922,15 +922,18 @@ func resourceNutanixVirtualMachineRead(d *schema.ResourceData, meta interface{})
 	b := make([]string, 0)
 
 	if resp.Status.Resources.BootConfig != nil {
-		if resp.Status.Resources.BootConfig.BootDevice.DiskAddress != nil {
-			i := strconv.Itoa(int(utils.Int64Value(resp.Status.Resources.BootConfig.BootDevice.DiskAddress.DeviceIndex)))
-			diskAddress["device_index"] = i
-			diskAddress["adapter_type"] = utils.StringValue(resp.Status.Resources.BootConfig.BootDevice.DiskAddress.AdapterType)
+		if resp.Status.Resources.BootConfig.BootDevice != nil {
+			if resp.Status.Resources.BootConfig.BootDevice.DiskAddress != nil {
+				i := strconv.Itoa(int(utils.Int64Value(resp.Status.Resources.BootConfig.BootDevice.DiskAddress.DeviceIndex)))
+				diskAddress["device_index"] = i
+				diskAddress["adapter_type"] = utils.StringValue(resp.Status.Resources.BootConfig.BootDevice.DiskAddress.AdapterType)
+			}
+			mac = utils.StringValue(resp.Status.Resources.BootConfig.BootDevice.MacAddress)
 		}
 		if resp.Status.Resources.BootConfig.BootDeviceOrderList != nil {
 			b = utils.StringValueSlice(resp.Status.Resources.BootConfig.BootDeviceOrderList)
 		}
-		mac = utils.StringValue(resp.Status.Resources.BootConfig.BootDevice.MacAddress)
+
 	}
 
 	d.Set("boot_device_order_list", b)
@@ -1090,7 +1093,7 @@ func resourceNutanixVirtualMachineUpdate(d *schema.ResourceData, meta interface{
 	if d.HasChange("num_vcpus_per_socket") {
 		o, n := d.GetChange("num_vcpus_per_socket")
 		res.NumVcpusPerSocket = utils.Int64Ptr(int64(n.(int)))
-		if n.(int) < o.(int) {
+		if n.(int) < o.(int) || n.(int) > o.(int) {
 			hotPlugChange = false
 		}
 	}
