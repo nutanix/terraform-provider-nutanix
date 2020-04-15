7e53b6cb96017e99b8684865f0f2558e33bcfa96
Edgar López <edgarlopez@pop-os.localdomain>
2020-04-15 16:20:27 -0500

    test: added testacc for data source of subnets

---
+++ nutanix/data_source_nutanix_subnets_test.go
@@ -0,0 +1,29 @@
+package nutanix
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccNutanixSubnetsDataSource_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSubnetsDataSourceConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.nutanix_subnets.test", "entities.0.cluster_reference.name"),
+				),
+			},
+		},
+	})
+}
+func testAccSubnetsDataSourceConfig() string {
+	return fmt.Sprintf(`
+data "nutanix_subnets" "test" {
+}
+`)
+}
