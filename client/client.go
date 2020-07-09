7d788811e6e548bdbe191984a91c03be162bbdde
Scott Grimes <scott-grimes@users.noreply.github.com>
2020-07-09 10:21:51 -0400

    Update client.go

--- client/client.go
+++ client/client.go
@@ -236,7 +236,7 @@ func CheckResponse(r *http.Response) error {
 		return nil
 	}
 	
-  // Nutanix returns non-json response with code 401 when
+        // Nutanix returns non-json response with code 401 when
 	// invalid credentials are used
 	if c == 401 {
 		return fmt.Errorf("Invalid Nutanix Credentials")
