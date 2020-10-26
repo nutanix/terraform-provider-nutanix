2ac9ea58ec02c063764e4b404674d454ad50d156
Yannick Struyf <yannick.struyf@nutanix.com>
2020-10-26 09:36:12 +0100

    modified code for Karbon 2.1.2

--- client/karbon/karbon_private_registry_structs.go
+++ client/karbon/karbon_private_registry_structs.go
@@ -9,10 +9,12 @@ type PrivateRegistryResponse struct {
 }
 
 type PrivateRegistryIntentInput struct {
-	Name *string `json:"name" mapstructure:"name, omitempty"`
-	Cert *string `json:"cert" mapstructure:"cert, omitempty"`
-	URL  *string `json:"url" mapstructure:"url, omitempty"`
-	Port *int64  `json:"port" mapstructure:"port, omitempty"`
+	Name     *string `json:"name" mapstructure:"name, omitempty"`
+	Cert     *string `json:"cert" mapstructure:"cert, omitempty"`
+	URL      *string `json:"url" mapstructure:"url, omitempty"`
+	Port     *int64  `json:"port" mapstructure:"port, omitempty"`
+	Username *string `json:"username" mapstructure:"username, omitempty"`
+	Password *string `json:"password" mapstructure:"password, omitempty"`
 }
 
 type PrivateRegistryOperationResponse struct {
