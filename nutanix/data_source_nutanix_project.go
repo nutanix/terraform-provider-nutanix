0ebc60264aba18c0d34576f6dda692a56e3cc3dd
Marin Salinas <marinssalinas@gmail.com>
2020-11-05 16:18:50 -0600

    fix: fix path for terraform sdk

--- nutanix/data_source_nutanix_project.go
+++ nutanix/data_source_nutanix_project.go
@@ -3,7 +3,7 @@ package nutanix
 import (
 	"fmt"
 
-	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
 func dataSourceNutanixProject() *schema.Resource {
