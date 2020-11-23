71103b614a6816b0b880539a2b0012a0c6721ae8
Edgar López <edgarlopez@pop-os.localdomain>
2020-11-23 14:25:19 -0700

    feat: added role name in datasource roles

--- nutanix/data_source_nutanix_role_test.go
+++ nutanix/data_source_nutanix_role_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
 
-func TestAccNutanixRoleDataSource_basic(t *testing.T) {
+func TestAccNutanixRoleDataSourceByID_basic(t *testing.T) {
 	name := acctest.RandomWithPrefix("accest-access-role")
 	description := "Description of my role"
 
@@ -18,7 +18,7 @@ func TestAccNutanixRoleDataSource_basic(t *testing.T) {
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccRoleDataSourceConfig(name, description),
+				Config: testAccRoleDataSourceConfigByID(name, description),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(
 						"data.nutanix_role.test", "name", name),
@@ -31,7 +31,29 @@ func TestAccNutanixRoleDataSource_basic(t *testing.T) {
 	})
 }
 
-func testAccRoleDataSourceConfig(name, description string) string {
+func TestAccNutanixRoleDataSourceByName_basic(t *testing.T) {
+	name := acctest.RandomWithPrefix("accest-access-role")
+	description := "Description of my role"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccRoleDataSourceConfigByID(name, description),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"data.nutanix_role.test", "name", name),
+					resource.TestCheckResourceAttr(
+						"data.nutanix_role.test", "description", description),
+					resource.TestCheckResourceAttrSet("data.nutanix_role.test", "name"),
+				),
+			},
+		},
+	})
+}
+
+func testAccRoleDataSourceConfigByID(name, description string) string {
 	return fmt.Sprintf(`
 resource "nutanix_role" "test" {
 	name        = "%[1]s"
@@ -44,6 +66,23 @@ resource "nutanix_role" "test" {
 
 data "nutanix_role" "test" {
 	role_id = nutanix_role.test.id
+}
+`, name, description)
+}
+
+func testAccRoleDataSourceConfigByName(name, description string) string {
+	return fmt.Sprintf(`
+resource "nutanix_role" "test" {
+	name        = "%[1]s"
+	description = "%[2]s"
+	permission_reference_list {
+		kind = "permission"
+		uuid = "2e9988df-47ae-44ae-9114-ada346657b90"
+	}
+}
+
+data "nutanix_role" "test" {
+	role_name = nutanix_role.test.name
 }
 `, name, description)
 }
