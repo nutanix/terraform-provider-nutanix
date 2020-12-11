fa1f9ab990f277f4f0798aeab657caed1c1ebc66
Yannick Struyf <yannick.struyf@nutanix.com>
2020-12-11 18:03:13 +0100

    added by name test for permission data source

--- nutanix/data_source_nutanix_permission_test.go
+++ nutanix/data_source_nutanix_permission_test.go
@@ -40,65 +40,30 @@ data "nutanix_permission" "test" {
 `, uuid)
 }
 
-// func TestAccNutanixPermissionDataSource_ByName(t *testing.T) {
+func TestAccNutanixPermissionDataSource_basicByName(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPermissionDataSourceConfigByName(PERMISSIONNAME),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"data.nutanix_permission.test", "name", PERMISSIONNAME),
+					resource.TestCheckResourceAttr(
+						"data.nutanix_permission.test", "operation", "console_access"),
+					resource.TestCheckResourceAttr(
+						"data.nutanix_permission.test", "fields.0.field_mode", "DISALLOWED"),
+				),
+			},
+		},
+	})
+}
 
-// 	resource.Test(t, resource.TestCase{
-// 		PreCheck:  func() { testAccPreCheck(t) },
-// 		Providers: testAccProviders,
-// 		Steps: []resource.TestStep{
-// 			{
-// 				Config: testAccPermissionDataSourceConfigByName(displayName),
-// 				Check: resource.ComposeTestCheckFunc(
-// 					resource.TestCheckResourceAttr(
-// 						"data.nutanix_permission.test", "display_name", displayName),
-// 					resource.TestCheckResourceAttr(
-// 						"data.nutanix_permission.test", "id", uuid),
-// 					resource.TestCheckResourceAttrSet("data.nutanix_permission.test", "directory_service_user_group.#"),
-// 					resource.TestCheckResourceAttr(
-// 						"data.nutanix_permission.test", "directory_service_user_group.0.distinguished_name", distinguishedName),
-// 				),
-// 			},
-// 		},
-// 	})
-// }
-
-// func testAccPermissionDataSourceConfigByName(dn string) string {
-// 	return fmt.Sprintf(`
-// data "nutanix_permission" "test" {
-// 	user_group_name = "%s"
-// }
-// `, dn)
-// }
-
-// func TestAccNutanixPermissionDataSource_ByDistinguishedName(t *testing.T) {
-// 	distinguishedName := "cn=dou-group-1,cn=users,dc=ntnxlab,dc=local"
-// 	displayName := "dou-group-1"
-// 	uuid := "d12fa0a3-13f1-4f5d-b773-c8e2f8144f0e"
-
-// 	resource.Test(t, resource.TestCase{
-// 		PreCheck:  func() { testAccPreCheck(t) },
-// 		Providers: testAccProviders,
-// 		Steps: []resource.TestStep{
-// 			{
-// 				Config: testAccPermissionDataSourceConfigByDistinguishedName(distinguishedName),
-// 				Check: resource.ComposeTestCheckFunc(
-// 					resource.TestCheckResourceAttr(
-// 						"data.nutanix_permission.test", "display_name", displayName),
-// 					resource.TestCheckResourceAttr(
-// 						"data.nutanix_permission.test", "id", uuid),
-// 					resource.TestCheckResourceAttrSet("data.nutanix_permission.test", "directory_service_user_group.#"),
-// 					resource.TestCheckResourceAttr(
-// 						"data.nutanix_permission.test", "directory_service_user_group.0.distinguished_name", distinguishedName),
-// 				),
-// 			},
-// 		},
-// 	})
-// }
-
-// func testAccPermissionDataSourceConfigByDistinguishedName(dn string) string {
-// 	return fmt.Sprintf(`
-// data "nutanix_permission" "test" {
-// 	user_group_distinguished_name = "%s"
-// }
-// `, dn)
-// }
+func testAccPermissionDataSourceConfigByName(name string) string {
+	return fmt.Sprintf(`
+data "nutanix_permission" "test" {
+	permission_name = "%s"
+}
+`, name)
+}
