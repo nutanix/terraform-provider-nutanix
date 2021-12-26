f04ba96a3e3e170184410f93571021e1bc477ecb
jan <jwalther@sva.de>
2021-12-26 23:19:30 +0100

    Add address group data sources

--- nutanix/provider.go
+++ nutanix/provider.go
@@ -117,6 +117,8 @@ func Provider() terraform.ResourceProvider {
 			"nutanix_protection_rules":          dataSourceNutanixProtectionRules(),
 			"nutanix_recovery_plan":             dataSourceNutanixRecoveryPlan(),
 			"nutanix_recovery_plans":            dataSourceNutanixRecoveryPlans(),
+			"nutanix_address_groups":            dataSourceNutanixAddressGroups(),
+			"nutanix_address_group":             dataSourceNutanixAddressGroup(),
 		},
 		ResourcesMap: map[string]*schema.Resource{
 			"nutanix_virtual_machine":         resourceNutanixVirtualMachine(),
