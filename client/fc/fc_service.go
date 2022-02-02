f671c09e09c5370c82576ed371050a65e4db90dd
shobhit <shobhit.khandelwal@nutanix.com>
2022-02-02 01:52:24 +0000

    Adding implementation for imagedNode APIs

--- client/fc/fc_service.go
+++ client/fc/fc_service.go
@@ -19,15 +19,43 @@ type Operations struct {
 
 // Service ...
 type Service interface {
-	GetVersion()
+	GetImagedNode(uuid string) (*ImagedNodeDetails, error)
+	ListImagedNodes(req *ImagedNodesListInput) (resp *ImagedNodesListResponse, error)
 	GetImagedCluster()
 	ListImagedCluster()
 	CreateCluster()
 	UpdateCluster()
 	DeleteCluster()
-	GetImagedNode()
-	ListImagedNodes()
 	CreateAPIKey()
-	ListAPIKeys()
 	GetAPIKey()
+	ListAPIKeys()
+}
+
+func (op Operations) GetImagedNode(nodeUUID string) (*ImagedNodeDetails, error) {
+	ctx := context.TODO()
+
+	path := fmt.Sprintf("/imaged_nodes/%s", uuid)
+
+	req, err := op.client.NewRequest(ctx, http.MethodGet, path, nil)
+	imagedNodeDetails := new(ImagedNodeDetails)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return imagedNodeDetails, op.client.Do(ctx, req, imagedNodeDetails)
+}
+
+func (op Operations) ListImagedNodes(req *ImagedNodesListInput) (*ImagedNodesListResponse, error) {
+	ctx := context.TODO()
+	path := "/imaged_nodes/list"
+
+	req, err := op.client.NewRequest(ctx, http.MethodPost, path, req)
+	imagedNodesListResponse := new(ImagedNodesListResponse)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return imagedNodesListResponse, op.client.Do(ctx, req, imagedNodesListResponse)
 }
