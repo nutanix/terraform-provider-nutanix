879c021fd41f71906f5500f0ae2a8a9c9cfe63fa
Pradeepsingh Bhati <pradeep.bhati@nutanix.com>
2022-02-15 23:37:38 +0530

    Fixing the existing examples and adding new.

---
+++ examples/security_rules/variables.tf
@@ -0,0 +1,19 @@
+#define the type of variables to be used in terraform file
+variable "nutanix_username" {
+  type = string
+}
+variable "nutanix_password" {
+  type = string
+}
+variable "nutanix_endpoint" {
+  type = string
+}
+variable "nutanix_port" {
+  type = string
+}
+variable "cluster_name" {
+  type = string
+}
+variable "subnet_name" {
+  type = string
+}
---
+++ examples/subnets_with_client_side_filters/terraform.tfvars
@@ -0,0 +1,5 @@
+#replace the values as per setup configuration
+nutanix_username = "admin"
+nutanix_password = "Nutanix/123456"
+nutanix_endpoint = "10.xx.xx.xx"
+nutanix_port = 9440
