ea68e51fe7aa3678b4c78e4ddf559d8686bf193d
bhatipradeep <bh.pradeepsingh@gmail.com>
2022-02-24 01:56:14 +0530

    1. [Add] Subnets datasource with filters test. 2. [Add] Recovery Plan datasource with name query test

--- nutanix/data_source_recovery_plan_test.go
+++ nutanix/data_source_recovery_plan_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func TestAccNutanixRecoveryPlanDataSource_basic(t *testing.T) {
+func TestAccRecoveryPlanDataSourceConfig_WithID(t *testing.T) {
 	resourceName := "nutanix_recovery_plan.test"
 
 	name := acctest.RandomWithPrefix("test-recovery-name-dou")
@@ -25,14 +25,14 @@ func TestAccNutanixRecoveryPlanDataSource_basic(t *testing.T) {
 		CheckDestroy: testAccCheckNutanixRecoveryPlanDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccRecoveryPlanDataSourceConfig(name, description, stageUUID),
+				Config: testAccRecoveryPlanDataSourceConfig_WithID(name, description, stageUUID),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceName, "name", name),
 					resource.TestCheckResourceAttr(resourceName, "description", description),
 				),
 			},
 			{
-				Config: testAccRecoveryPlanDataSourceConfig(nameUpdated, descriptionUpdated, stageUUID),
+				Config: testAccRecoveryPlanDataSourceConfig_WithID(nameUpdated, descriptionUpdated, stageUUID),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceName, "name", nameUpdated),
 					resource.TestCheckResourceAttr(resourceName, "description", descriptionUpdated),
@@ -42,7 +42,39 @@ func TestAccNutanixRecoveryPlanDataSource_basic(t *testing.T) {
 	})
 }
 
-func testAccRecoveryPlanDataSourceConfig(name, description, stageUUID string) string {
+func TestAccRecoveryPlanDataSourceConfig_WithName(t *testing.T) {
+
+	name := acctest.RandomWithPrefix("test-recovery-name")
+	nameUpdated := acctest.RandomWithPrefix("test-recovery-name")
+	description := acctest.RandomWithPrefix("test-recovery-desc")
+	descriptionUpdated := acctest.RandomWithPrefix("test-recovery-desc")
+	stageUUID := "bb261302-94db-11ec-b909-0242ac120002"
+
+	resourceName := "nutanix_recovery_plan.test"
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckNutanixRecoveryPlanDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccRecoveryPlanDataSourceConfig_WithName(name, description, stageUUID),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "description", description),
+				),
+			},
+			{
+				Config: testAccRecoveryPlanDataSourceConfig_WithName(nameUpdated, descriptionUpdated, stageUUID),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", nameUpdated),
+					resource.TestCheckResourceAttr(resourceName, "description", descriptionUpdated),
+				),
+			},
+		},
+	})
+}
+
+func testAccRecoveryPlanDataSourceConfig_WithID(name, description, stageUUID string) string {
 	return fmt.Sprintf(`
         resource "nutanix_recovery_plan" "test" {
 			name        = "%s"
@@ -65,6 +97,34 @@ func testAccRecoveryPlanDataSourceConfig(name, description, stageUUID string) st
 		}
 		data "nutanix_recovery_plan" "test" {
 			recovery_plan_id = nutanix_recovery_plan.test.id
+		}
+
+`, name, description, stageUUID)
+}
+
+func testAccRecoveryPlanDataSourceConfig_WithName(name, description, stageUUID string) string {
+	return fmt.Sprintf(`
+        resource "nutanix_recovery_plan" "test" {
+			name        = "%s"
+			description = "%s"
+			stage_list {
+				stage_work{
+					recover_entities{
+						entity_info_list{
+							categories {
+								name = "Environment"
+								value = "Dev"
+							}
+						}
+					}
+				}
+				stage_uuid = "%s"
+				delay_time_secs = 0
+			}
+			parameters{}
+		}
+		data "nutanix_recovery_plan" "test" {
+			recovery_plan_name = nutanix_recovery_plan.test.name
 		}
 
 `, name, description, stageUUID)
