d4c6efc400be7551ad7ea2a8383d67218ad077a9
bhatipradeep <bh.pradeepsingh@gmail.com>
2022-02-28 01:40:25 +0530

    1. [Add] data source read func for setting resource data as per api response

--- nutanix/data_source_foundation_discover_nodes.go
+++ nutanix/data_source_foundation_discover_nodes.go
@@ -4,7 +4,9 @@ import (
 	"context"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/terraform-providers/terraform-provider-nutanix/client/foundation"
 )
 
 func dataSourceFoundationDiscoverNodes() *schema.Resource {
@@ -20,8 +22,8 @@ func dataSourceFoundationDiscoverNodes() *schema.Resource {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
-						"chasis_n": {
-							Type:     schema.TypeString,
+						"chassis_n": {
+							Type:     schema.TypeInt,
 							Computed: true,
 						},
 						"block_id": {
@@ -77,7 +79,7 @@ func dataSourceFoundationDiscoverNodes() *schema.Resource {
 										Type:     schema.TypeString,
 										Computed: true,
 									},
-									"cvm_ip": {
+									"svm_ip": {
 										Type:     schema.TypeString,
 										Computed: true,
 									},
@@ -95,6 +97,61 @@ func dataSourceFoundationDiscoverNodes() *schema.Resource {
 	}
 }
 
-func dataSourceFoundationDiscoverNodesRead(ctx context.Context, d *schema.ResourceData, mata interface{}) diag.Diagnostics {
+func dataSourceFoundationDiscoverNodesRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+
+	conn := meta.(*Client).FoundationClientAPI
+
+	resp, err := conn.Networking.DiscoverNodes(ctx)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	entities := make([]map[string]interface{}, len(*resp))
+	for k, v := range *resp {
+		entity := make(map[string]interface{})
+		entity["model"] = v.Model
+		entity["chassis_n"] = v.ChassisN
+		entity["block_id"] = v.BlockID
+
+		entity["nodes"] = flattenDiscoveredNodes(v.Nodes)
+
+		entities[k] = entity
+	}
+	// log.Println(entities)
+	setErr := d.Set("entities", entities)
+	if setErr != nil {
+		return diag.FromErr(err)
+	}
+	d.SetId(resource.UniqueId())
 	return nil
+}
+
+func flattenDiscoveredNodes(nodesList []foundation.DiscoveredNode) []map[string]interface{} {
+	nodes := make([]map[string]interface{}, len(nodesList))
+	for k, v := range nodesList {
+		node := make(map[string]interface{})
+
+		node["foundation_version"] = v.FoundationVersion
+		node["ipv6_address"] = v.Ipv6Address
+		node["node_uuid"] = v.NodeUUID
+		node["current_network_interface"] = v.CurrentNetworkInterface
+		node["node_position"] = v.NodePosition
+		node["hypervisor"] = v.Hypervisor
+		node["configured"] = v.Configured
+		node["nos_version"] = v.NosVersion
+
+		if v.ClusterID != nil && v.ClusterID != "" {
+			node["cluster_id"] = int64((v.ClusterID).(float64))
+		} else {
+			node["cluster_id"] = int64(0)
+		}
+
+		node["current_cvm_vlan_tag"] = v.CurrentCvmVlanTag
+
+		node["hypervisor_version"] = v.HypervisorVersion
+		node["svm_ip"] = v.SvmIP
+		node["model"] = v.Model
+
+		nodes[k] = node
+	}
+	return nodes
 }
