5a2b64564d7227206910eb7f1d4c2576dbf0fe15
bhatipradeep <bh.pradeepsingh@gmail.com>
2022-03-02 17:18:40 +0530

    1.[Update] Comments

--- client/foundation/foundation_file_management_service.go
+++ client/foundation/foundation_file_management_service.go
@@ -7,16 +7,18 @@ import (
 	"github.com/terraform-providers/terraform-provider-nutanix/client"
 )
 
+// Interface for file management apis of foundation
 type FileManagementService interface {
 	ListNOSPackages(context.Context) (*ListNOSPackagesResponse, error)
 	ListHypervisorISOs(context.Context) (*ListHypervisorISOsResponse, error)
 }
 
+// FileManagementOperations implements FileManagementService interface
 type FileManagementOperations struct {
 	client *client.Client
 }
 
-//Lists the available AOS packages in Foundation
+//ListNOSPackages lists the available AOS packages in Foundation
 func (fmo FileManagementOperations) ListNOSPackages(ctx context.Context) (*ListNOSPackagesResponse, error) {
 	path := "/enumerate_nos_packages"
 	req, err := fmo.client.NewUnAuthRequest(ctx, http.MethodGet, path, nil)
@@ -27,7 +29,7 @@ func (fmo FileManagementOperations) ListNOSPackages(ctx context.Context) (*ListN
 	return listNOSPackagesResponse, fmo.client.Do(ctx, req, listNOSPackagesResponse)
 }
 
-//Lists the hypervisor ISOs available in Foundation
+//ListHypervisorISOs lists the hypervisor ISOs available in Foundation
 func (fmo FileManagementOperations) ListHypervisorISOs(ctx context.Context) (*ListHypervisorISOsResponse, error) {
 	path := "/enumerate_hypervisor_isos"
 	req, err := fmo.client.NewUnAuthRequest(ctx, http.MethodGet, path, nil)
