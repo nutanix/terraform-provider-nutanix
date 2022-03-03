af39b56263c35fb109964a5567faa9911ab714b0
bhatipradeep <bh.pradeepsingh@gmail.com>
2022-03-03 11:15:30 +0530

    rebase

--- nutanix/provider.go
+++ nutanix/provider.go
@@ -136,6 +136,7 @@ func Provider() *schema.Provider {
 			"nutanix_address_group":              dataSourceNutanixAddressGroup(),
 			"nutanix_foundation_hypervisor_isos": dataSourceFoundationHypervisorIsos(),
 			"nutanix_foundation_discover_nodes":  dataSourceFoundationDiscoverNodes(),
+			"nutanix_foundation_nos_packages":    dataSourceFoundationNOSPackages(),
 		},
 		ResourcesMap: map[string]*schema.Resource{
 			"nutanix_virtual_machine":         resourceNutanixVirtualMachine(),
