c5e8c1088a763d2bb406be9bd5f74e4377e18e67
Abhishek <abhishekism9450@gmail.com>
2022-06-20 23:35:14 +0530

    PBR datasource and resource

--- nutanix/provider.go
+++ nutanix/provider.go
@@ -169,6 +169,7 @@ func Provider() *schema.Provider {
 			"nutanix_static_routes":                           dataSourceNutanixStaticRoute(),
 			"nutanix_floating_ip":                             dataSourceNutanixFloatingIP(),
 			"nutanix_floating_ips":                            dataSourceNutanixFloatingIPs(),
+			"nutanix_pbr":                                     dataSourceNutanixPbr(),
 		},
 		ResourcesMap: map[string]*schema.Resource{
 			"nutanix_virtual_machine":                  resourceNutanixVirtualMachine(),
@@ -197,6 +198,7 @@ func Provider() *schema.Provider {
 			"nutanix_floating_ip":                      resourceNutanixFloatingIP(),
 			"nutanix_static_routes":                    resourceNutanixStaticRoute(),
 			"nutanix_floating_ip":                      resourceNutanixFloatingIP(),
+			"nutanix_pbr":                              resourceNutanixPbr(),
 		},
 		ConfigureContextFunc: providerConfigure,
 	}
