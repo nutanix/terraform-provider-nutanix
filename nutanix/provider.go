f76d1753f0e07aa82d31f0c10e41cbfaef89e54b
Abhishek <abhishekism9450@gmail.com>
2022-06-27 15:42:55 +0530

    change datasource naming convention

--- nutanix/provider.go
+++ nutanix/provider.go
@@ -161,7 +161,7 @@ func Provider() *schema.Provider {
 			"nutanix_foundation_central_cluster_details":      dataSourceNutanixFCClusterDetails(),
 			"nutanix_foundation_central_imaged_node_details":  dataSourceFCImagedNodeDetails(),
 			"nutanix_vpc":                                     dataSourceNutanixVPC(),
-			"nutanix_vpc_list":                                dataSourceNutanixVPCList(),
+			"nutanix_vpcs":                                    dataSourceNutanixVPCs(),
 			"nutanix_pbr":                                     dataSourceNutanixPbr(),
 		},
 		ResourcesMap: map[string]*schema.Resource{
