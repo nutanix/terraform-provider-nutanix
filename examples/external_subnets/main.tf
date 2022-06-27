47dab7863cbd06b63e381897b49fc6ba859537de
Abhishek <abhishekism9450@gmail.com>
2022-06-27 11:24:00 +0530

    added testcases for subnets external and overlay

--- examples/external_subnets/main.tf
+++ examples/external_subnets/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     nutanix = {
-      source  = "nutanixtemp/nutanix"
-      version = "1.99.99"
+      source  = "nutanix/nutanix"
+      version = "1.6.0"
     }
   }
 }
@@ -28,47 +28,47 @@ locals {
 # External Subnets with NAT
 
 resource "nutanix_subnet" "accNat" {
-    # General Info
-	name        = "test-ext-sub-with-nat"
-	description = "Description of my test VLAN updated"
+  # General Info
+  name        = "test-ext-sub-with-nat"
+  description = "Description of my test VLAN updated"
 
-    # subnet_type should be VLAN for external subnet with NAT
-	subnet_type = "VLAN"
-    cluster_uuid = local.cluster1
-    vlan_id = 121
-	subnet_ip          = "10.xx.xx.xx"
-    default_gateway_ip = "10.xx.xx.xx"
-    prefix_length = 24
+  # subnet_type should be VLAN for external subnet with NAT
+  subnet_type = "VLAN"
+  cluster_uuid = local.cluster1
+  vlan_id = 121
+  subnet_ip          = "10.xx.xx.xx"
+  default_gateway_ip = "10.xx.xx.xx"
+  prefix_length = 24
 
-    # required to be set true for external connectivity
-    is_external = true
-    # set true if NAT reuired
-    enable_nat = true
+  # required to be set true for external connectivity
+  is_external = true
+  # set true if NAT reuired
+  enable_nat = true
 
-    ip_config_pool_list_ranges = ["10.xx.xx.xx 10.xx.xx.xx"]
+  ip_config_pool_list_ranges = ["10.xx.xx.xx 10.xx.xx.xx"]
  
 }
 
 # External Subnet with No NAT
 
 resource "nutanix_subnet" "accNoNat" {
-    # General Info
-	name        = "test-ext-sub-with-no-nat"
-	description = "Description of my test VLAN updated"
+  # General Info
+  name        = "test-ext-sub-with-no-nat"
+  description = "Description of my test VLAN updated"
 
-    # subnet_type should be VLAN for external subnet with No NAT
-	subnet_type = "VLAN"
-    cluster_uuid = local.cluster1
-    vlan_id = 121
-	subnet_ip          = "10.xx.xx.xx"
-    default_gateway_ip = "10.xx.xx.xx"
-    prefix_length = 24
+  # subnet_type should be VLAN for external subnet with No NAT
+  subnet_type = "VLAN"
+  cluster_uuid = local.cluster1
+  vlan_id = 121
+  subnet_ip          = "10.xx.xx.xx"
+  default_gateway_ip = "10.xx.xx.xx"
+  prefix_length = 24
 
-    # required to be set true for external connectivity
-    is_external = true
-    # set fasle for No NAT
-    enable_nat = false
+  # required to be set true for external connectivity
+  is_external = true
+  # set fasle for No NAT
+  enable_nat = false
 
-    ip_config_pool_list_ranges = ["10.xx.xx.xx 10.xx.xx.xx"]
+  ip_config_pool_list_ranges = ["10.xx.xx.xx 10.xx.xx.xx"]
  
 }
