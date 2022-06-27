995fb1cb6a62b10d5692ad378b18175cbd2c3064
Abhishek <abhishekism9450@gmail.com>
2022-06-27 17:31:36 +0530

    datasource for pbr list

--- client/v3/v3_structs.go
+++ client/v3/v3_structs.go
@@ -2745,3 +2745,9 @@ type PbrIntentResponse struct {
 
 	Status *PbrDefStatus `json:"status"`
 }
+
+type PbrListIntentResponse struct {
+	APIVersion *string              `json:"api_version"`
+	Metadata   *ListMetadataOutput  `json:"metadata"`
+	Entities   []*PbrIntentResponse `json:"entities"`
+}
