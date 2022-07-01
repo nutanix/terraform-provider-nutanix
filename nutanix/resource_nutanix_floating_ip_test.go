f11215d0c72ddca6c9423303d22637bdcabbf49c
Abhishek <abhishekism9450@gmail.com>
2022-07-01 18:34:50 +0530

    added more testcases

--- nutanix/resource_nutanix_floating_ip_test.go
+++ nutanix/resource_nutanix_floating_ip_test.go
@@ -67,6 +67,28 @@ func TestAccNutanixFloatingIP_WithVPCName(t *testing.T) {
 	})
 }
 
+func TestAccNutanixFloatingIP_WithSubnetName(t *testing.T) {
+	r := randIntBetween(35, 50)
+	vpcName := fmt.Sprintf("acctest-vpc-%d", r)
+	subnetName := fmt.Sprintf("acctest-subnet-%d", r)
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckNutanixFloatingIPDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNutanixFloatingIPConfigWithSubnetName(r),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceNameFloatingIP, "external_subnet_reference_uuid"),
+					resource.TestCheckResourceAttr(resourceNameFloatingIP, "vpc_reference_name", vpcName),
+					resource.TestCheckResourceAttr(resourceNameFloatingIP, "external_subnet_reference_name", subnetName),
+					resource.TestCheckResourceAttr(resourceNameFloatingIP, "private_ip", "10.3.3.6"),
+				),
+			},
+		},
+	})
+}
+
 func testAccCheckNutanixFloatingIPDestroy(s *terraform.State) error {
 	conn := testAccProvider.Meta().(*Client)
 	ctx := context.TODO()
@@ -212,6 +234,61 @@ func testAccNutanixFloatingIPConfigWithVpcName(r int) string {
 
 	resource "nutanix_floating_ip" "acctest-managed" {
 		external_subnet_reference_uuid = resource.nutanix_subnet.sub-test.id
+		vpc_reference_name = resource.nutanix_vpc.test-vpc.name
+		private_ip = "10.3.3.6"
+
+		depends_on = [
+			resource.nutanix_vpc.test-vpc
+		]
+	}
+	`, r)
+}
+
+func testAccNutanixFloatingIPConfigWithSubnetName(r int) string {
+	return fmt.Sprintf(`
+
+	data "nutanix_clusters" "clusters" {}
+	
+	locals {
+		cluster1 = [
+		for cluster in data.nutanix_clusters.clusters.entities :
+		cluster.metadata.uuid if cluster.service_list[0] != "PRISM_CENTRAL"
+		][0]
+	}
+	
+	resource "nutanix_subnet" "sub-test" {
+		cluster_uuid = local.cluster1
+		name        = "acctest-subnet-%[1]d"
+		description = "Description of my unit test VLAN"
+		vlan_id     = %[1]d
+		subnet_type = "VLAN"
+		subnet_ip          = "10.250.140.0"
+	  default_gateway_ip = "10.250.140.1"
+	  prefix_length = 24
+	  is_external = true
+	  ip_config_pool_list_ranges = ["10.250.140.10 10.250.140.20"]
+	}
+
+	resource "nutanix_vpc" "test-vpc" {
+		name = "acctest-vpc-%[1]d"
+	  
+	  
+		external_subnet_reference_uuid = [
+		  resource.nutanix_subnet.sub-test.id
+		]
+	  
+		common_domain_name_server_ip_list{
+				ip = "8.8.8.9"
+		}
+	  
+		externally_routable_prefix_list{
+		  ip=  "172.31.0.0"
+		  prefix_length= 16
+		}
+	}
+
+	resource "nutanix_floating_ip" "acctest-managed" {
+		external_subnet_reference_name = "acctest-subnet-%[1]d"
 		vpc_reference_name = resource.nutanix_vpc.test-vpc.name
 		private_ip = "10.3.3.6"
 
