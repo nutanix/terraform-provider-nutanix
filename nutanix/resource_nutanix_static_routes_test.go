fe25f4ad1ee54308e7e5a17fcf5268180a64c55f
Abhishek <abhishekism9450@gmail.com>
2022-07-01 18:34:50 +0530

    added more testcases

--- nutanix/resource_nutanix_static_routes_test.go
+++ nutanix/resource_nutanix_static_routes_test.go
@@ -51,6 +51,25 @@ func TestAccNutanixStaticRoutes_Update(t *testing.T) {
 	})
 }
 
+func TestAccNutanixStaticRoutes_WithVpcName(t *testing.T) {
+	r := randIntBetween(51, 60)
+	vpcName := fmt.Sprintf("acctest-vpc-%d", r)
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNutanixStaticRoutesConfigWithVpcName(r),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceNameStaticRoute, "vpc_name", vpcName),
+					resource.TestCheckResourceAttr(resourceNameStaticRoute, "static_routes_list.#", "1"),
+					resource.TestCheckResourceAttr(resourceNameStaticRoute, "static_routes_list.0.destination", "10.2.2.0/24"),
+				),
+			},
+		},
+	})
+}
+
 func testAccNutanixStaticRoutesConfig(r int) string {
 	return fmt.Sprintf(`
 	data "nutanix_clusters" "clusters" {}
@@ -156,6 +175,62 @@ func testAcctestAccNutanixStaticRoutesConfigUpdate(r int) string {
 			external_subnet_reference_uuid = resource.nutanix_subnet.ext-sub.id
 		}
 
+	}
+	`, r)
+}
+
+func testAccNutanixStaticRoutesConfigWithVpcName(r int) string {
+	return fmt.Sprintf(`
+	data "nutanix_clusters" "clusters" {}
+	
+	locals {
+		cluster1 = [
+		for cluster in data.nutanix_clusters.clusters.entities :
+		cluster.metadata.uuid if cluster.service_list[0] != "PRISM_CENTRAL"
+		][0]
+	}
+	
+	resource "nutanix_subnet" "ext-sub" {
+		cluster_uuid = local.cluster1
+		name        = "acctest-managed-%[1]d"
+		description = "Description of my unit test VLAN"
+		vlan_id     = %[1]d
+		subnet_type = "VLAN"
+		subnet_ip          = "10.250.140.0"
+		default_gateway_ip = "10.250.140.1"
+		prefix_length = 24
+		is_external = true
+	  	ip_config_pool_list_ranges = ["10.250.140.10 10.250.140.20"]
+	}
+
+	resource "nutanix_vpc" "test-vpc" {
+		name = "acctest-vpc-%[1]d"
+	  
+	  
+		external_subnet_reference_uuid = [
+		  resource.nutanix_subnet.ext-sub.id
+		]
+	  
+		common_domain_name_server_ip_list{
+				ip = "8.8.8.9"
+		}
+	  
+		externally_routable_prefix_list{
+		  ip=  "174.28.0.0"
+		  prefix_length= 16
+		}
+	  }
+
+	resource "nutanix_static_routes" "acctest-managed"{
+		depends_on = [
+			resource.nutanix_vpc.test-vpc
+		]
+		vpc_name = "acctest-vpc-%[1]d"
+
+		static_routes_list{
+			destination= "10.2.2.0/24"
+			external_subnet_reference_uuid = resource.nutanix_subnet.ext-sub.id
+		}
 	}
 	`, r)
 }
--- nutanix/resource_nutanix_vpc_test.go
+++ nutanix/resource_nutanix_vpc_test.go
@@ -64,6 +64,28 @@ func TestAccNutanixVpc_Update(t *testing.T) {
 	})
 }
 
+func TestAccNutanixVpc_WithSubnetName(t *testing.T) {
+	r := randIntBetween(31, 40)
+	vpcName := fmt.Sprintf("acctest-managed-%d", r)
+	subnetName := fmt.Sprintf("acctest-subnet-%d", r)
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckNutanixVpcDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNutanixVpcConfigWithSubnetName(r),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceNameVpc, "name", vpcName),
+					resource.TestCheckResourceAttr(resourceNameVpc, "external_subnet_reference_name.0", subnetName),
+					resource.TestCheckResourceAttr(resourceNameVpc, "common_domain_name_server_ip_list.#", "1"),
+					resource.TestCheckResourceAttr(resourceNameVpc, "externally_routable_prefix_list.#", "1"),
+				),
+			},
+		},
+	})
+}
+
 func testAccCheckNutanixVpcDestroy(s *terraform.State) error {
 	conn := testAccProvider.Meta().(*Client)
 	ctx := context.TODO()
@@ -176,4 +198,50 @@ resource "nutanix_vpc" "acctest-managed" {
 
 
 `, r)
+}
+
+func testAccNutanixVpcConfigWithSubnetName(r int) string {
+	return fmt.Sprintf(`
+	data "nutanix_clusters" "clusters" {}
+	
+	locals {
+		cluster1 = [
+		for cluster in data.nutanix_clusters.clusters.entities :
+		cluster.metadata.uuid if cluster.service_list[0] != "PRISM_CENTRAL"
+		][0]
+	}
+	
+	resource "nutanix_subnet" "acctest-managed" {
+		cluster_uuid = local.cluster1
+		name        = "acctest-subnet-%[1]d"
+		description = "Description of my unit test VLAN"
+		vlan_id     = %[1]d
+		subnet_type = "VLAN"
+		subnet_ip          = "10.250.144.0"
+	  default_gateway_ip = "10.250.144.1"
+	  prefix_length = 24
+	  is_external = true
+	  ip_config_pool_list_ranges = ["10.250.144.10 10.250.144.20"]
+	}
+
+	resource "nutanix_vpc" "acctest-managed" {
+		depends_on = [
+			resource.nutanix_subnet.acctest-managed
+		]
+		name = "acctest-managed-%[1]d"
+	  
+		external_subnet_reference_name = [
+		  "acctest-subnet-%[1]d"
+		]
+	  
+		common_domain_name_server_ip_list{
+				ip = "8.8.8.9"
+		}
+	  
+		externally_routable_prefix_list{
+		  ip=  "172.30.0.0"
+		  prefix_length= 16
+		}
+	  }
+	`, r)
 }
