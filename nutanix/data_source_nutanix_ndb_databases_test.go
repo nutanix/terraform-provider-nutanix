9b9d88f07d178363c89b68846c702196d613d03f
Abhishekism9450 <32683845+Abhishekism9450@users.noreply.github.com>
2023-01-03 15:50:37 +0530

    filter based on database type (#513)

--- nutanix/data_source_nutanix_ndb_databases_test.go
+++ nutanix/data_source_nutanix_ndb_databases_test.go
@@ -25,8 +25,35 @@ func TestAccEraDatabasesDataSource_basic(t *testing.T) {
 	})
 }
 
+func TestAccEraDatabasesDataSource_ByFilters(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccEraPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccEraDatabasesDataSourceConfigByFilters(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.nutanix_ndb_databases.test", "database_instances.0.metadata.#"),
+					resource.TestCheckResourceAttrSet("data.nutanix_ndb_databases.test", "database_instances.0.time_zone"),
+					resource.TestCheckResourceAttrSet("data.nutanix_ndb_databases.test", "database_instances.#"),
+					resource.TestCheckResourceAttrSet("data.nutanix_ndb_databases.test", "database_instances.0.id"),
+					resource.TestCheckResourceAttrSet("data.nutanix_ndb_databases.test", "database_instances.0.linked_databases.#"),
+				),
+			},
+		},
+	})
+}
+
 func testAccEraDatabasesDataSourceConfig() string {
 	return `
 	data "nutanix_ndb_databases" "test" {}
+`
+}
+
+func testAccEraDatabasesDataSourceConfigByFilters() string {
+	return `
+	data "nutanix_ndb_databases" "test" {
+		database_type = "postgres_database"
+	}
 `
 }
