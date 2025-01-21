9d010aae32e9a16498d7bdd5c871c7525b815205
Haroon-Dweikat-Ntx <haroon.dweikat@nutanix.com>
2025-01-21 15:44:47 +0200

    remove unnecessary files

--- nutanix/services/datapoliciesv2/helper.go
+++
@@ -1,9 +0,0 @@
-package datapoliciesv2
-
-func expandListOfString(list []interface{}) []string {
-	stringListStr := make([]string, len(list))
-	for i, v := range list {
-		stringListStr[i] = v.(string)
-	}
-	return stringListStr
-}
--- nutanix/services/dataprotectionv2/resource_nutanix_restore_protected_resource_v2_test.go
+++
@@ -1,1 +0,0 @@
-package dataprotectionv2_test
