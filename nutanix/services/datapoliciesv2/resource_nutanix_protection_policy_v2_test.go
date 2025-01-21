0f459bf7cd0b92b4c35f8ac8fe2a4b4c07aee39c
Haroon-Dweikat-Ntx <haroon.dweikat@nutanix.com>
2025-01-21 15:05:02 +0200

    lint fixes

--- nutanix/services/datapoliciesv2/resource_nutanix_protection_policy_v2_test.go
+++ nutanix/services/datapoliciesv2/resource_nutanix_protection_policy_v2_test.go
@@ -2,10 +2,11 @@ package datapoliciesv2_test
 
 import (
 	"fmt"
+	"testing"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	acc "github.com/terraform-providers/terraform-provider-nutanix/nutanix/acctest"
-	"testing"
 )
 
 const resourceNameProtectionPolicy = "nutanix_protection_policy_v2.test"
