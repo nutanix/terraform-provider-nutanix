0caf8846b6b6e615c214e637bb8473ce28b3b2b2
Gullapalli Akhil Sai <akhilsai.gullapalli@nutanix.com>
2025-02-17 04:33:38 +0000

    LCM config and inventory changes

--- nutanix/services/lcmv2/resource_lcm_inventory_v2.go
+++ nutanix/services/lcmv2/resource_lcm_inventory_v2.go
@@ -2,12 +2,17 @@ package lcmv2
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	taskRef "github.com/nutanix/ntnx-api-golang-clients/clustermgmt-go-client/v4/models/prism/v4/config"
 	lcmInventoryResp "github.com/nutanix/ntnx-api-golang-clients/lcm-go-client/v4/models/lcm/v4/operations"
+	import1 "github.com/nutanix/ntnx-api-golang-clients/prism-go-client/v4/models/prism/v4/config"
 	conns "github.com/terraform-providers/terraform-provider-nutanix/nutanix"
+	"github.com/terraform-providers/terraform-provider-nutanix/nutanix/sdks/v4/prism"
+	"github.com/terraform-providers/terraform-provider-nutanix/utils"
 )
 
 func ResourceLcmInventoryV2() *schema.Resource {
@@ -49,6 +54,62 @@ func ResourceLcmPerformInventoryV2(ctx context.Context, d *schema.ResourceData, 
 	TaskRef := getResp.Data.GetValue().(taskRef.TaskReference)
 	taskUUID := TaskRef.ExtId
 
-	d.Set("ext_id", taskUUID)
+	// calling group API to poll for completion of task
+
+	taskconn := meta.(*conns.Client).PrismAPI
+	// Wait for the inventorty to be successful
+	stateConf := &resource.StateChangeConf{
+		Pending: []string{"QUEUED", "RUNNING", "PENDING"},
+		Target:  []string{"SUCCEEDED"},
+		Refresh: taskStateRefreshPrismTaskGroupFunc(ctx, taskconn, utils.StringValue(taskUUID)),
+		Timeout: d.Timeout(schema.TimeoutCreate),
+	}
+
+	if _, errWaitTask := stateConf.WaitForStateContext(ctx); errWaitTask != nil {
+		return diag.Errorf("Perform inventory task failed: %s", errWaitTask)
+	}
 	return nil
+}
+
+func taskStateRefreshPrismTaskGroupFunc(ctx context.Context, client *prism.Client, taskUUID string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		// data := base64.StdEncoding.EncodeToString([]byte("ergon"))
+		// encodeUUID := data + ":" + taskUUID
+		vresp, err := client.TaskRefAPI.GetTaskById(utils.StringPtr(taskUUID), nil)
+		if err != nil {
+			return "", "", (fmt.Errorf("error while polling prism task: %v", err))
+		}
+
+		// get the group results
+
+		v := vresp.Data.GetValue().(import1.Task)
+
+		if getTaskStatus(v.Status) == "CANCELED" || getTaskStatus(v.Status) == "FAILED" {
+			return v, getTaskStatus(v.Status),
+				fmt.Errorf("error_detail: %s, progress_message: %d", utils.StringValue(v.ErrorMessages[0].Message), utils.IntValue(v.ProgressPercentage))
+		}
+		return v, getTaskStatus(v.Status), nil
+	}
+}
+
+func getTaskStatus(pr *import1.TaskStatus) string {
+	const two, three, five, six, seven = 2, 3, 5, 6, 7
+	if pr != nil {
+		if *pr == import1.TaskStatus(six) {
+			return "FAILED"
+		}
+		if *pr == import1.TaskStatus(seven) {
+			return "CANCELED"
+		}
+		if *pr == import1.TaskStatus(two) {
+			return "QUEUED"
+		}
+		if *pr == import1.TaskStatus(three) {
+			return "RUNNING"
+		}
+		if *pr == import1.TaskStatus(five) {
+			return "SUCCEEDED"
+		}
+	}
+	return "UNKNOWN"
 }
--- nutanix/services/lcmv2/resource_lcm_prechecks_v2.go
+++ nutanix/services/lcmv2/resource_lcm_prechecks_v2.go
@@ -1,15 +1,19 @@
 package lcmv2
 
 import (
-	"container/list"
 	"context"
+	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	taskRef "github.com/nutanix/ntnx-api-golang-clients/clustermgmt-go-client/v4/models/prism/v4/config"
 	preCheckConfig "github.com/nutanix/ntnx-api-golang-clients/lcm-go-client/v4/models/lcm/v4/common"
 	lcmPreCheckResp "github.com/nutanix/ntnx-api-golang-clients/lcm-go-client/v4/models/lcm/v4/operations"
+	import1 "github.com/nutanix/ntnx-api-golang-clients/prism-go-client/v4/models/prism/v4/config"
 	conns "github.com/terraform-providers/terraform-provider-nutanix/nutanix"
+	"github.com/terraform-providers/terraform-provider-nutanix/nutanix/sdks/v4/prism"
+	"github.com/terraform-providers/terraform-provider-nutanix/utils"
 )
 
 func ResourcePreChecksV2() *schema.Resource {
@@ -86,15 +90,73 @@ func ResourceLcmPreChecksV2(ctx context.Context, d *schema.ResourceData, meta in
 	args := make(map[string]interface{})
 	args["X-Cluster-Id"] = clusterId
 	args["NTNX-Request-Id"] = ntnxRequestId
+	body := preCheckConfig.PrecheckSpec{}
 
-	resp, err := conn.LcmPreChecksAPIInstance.Precheck(body, args)
+	resp, err := conn.LcmPreChecksAPIInstance.Precheck(&body, args)
 	if err != nil {
-		return diag.Errorf("error while performing the inventory: %v", err)
+		return diag.Errorf("error while performing the prechecs: %v", err)
 	}
 	getResp := resp.Data.GetValue().(lcmPreCheckResp.PrecheckApiResponse)
 	TaskRef := getResp.Data.GetValue().(taskRef.TaskReference)
 	taskUUID := TaskRef.ExtId
 
-	d.Set("ext_id", taskUUID)
+	// calling group API to poll for completion of task
+
+	taskconn := meta.(*conns.Client).PrismAPI
+	// Wait for the inventorty to be successful
+	stateConf := &resource.StateChangeConf{
+		Pending: []string{"QUEUED", "RUNNING", "PENDING"},
+		Target:  []string{"SUCCEEDED"},
+		Refresh: taskStateRefreshPrismTaskGroupFun(ctx, taskconn, utils.StringValue(taskUUID)),
+		Timeout: d.Timeout(schema.TimeoutCreate),
+	}
+
+	if _, errWaitTask := stateConf.WaitForStateContext(ctx); errWaitTask != nil {
+		return diag.Errorf("Prechecks task failed: %s", errWaitTask)
+	}
 	return nil
+
+}
+
+func taskStateRefreshPrismTaskGroupFun(ctx context.Context, client *prism.Client, taskUUID string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		// data := base64.StdEncoding.EncodeToString([]byte("ergon"))
+		// encodeUUID := data + ":" + taskUUID
+		vresp, err := client.TaskRefAPI.GetTaskById(utils.StringPtr(taskUUID), nil)
+		if err != nil {
+			return "", "", (fmt.Errorf("error while polling prism task: %v", err))
+		}
+
+		// get the group results
+
+		v := vresp.Data.GetValue().(import1.Task)
+
+		if getTaskStat(v.Status) == "CANCELED" || getTaskStat(v.Status) == "FAILED" {
+			return v, getTaskStat(v.Status),
+				fmt.Errorf("error_detail: %s, progress_message: %d", utils.StringValue(v.ErrorMessages[0].Message), utils.IntValue(v.ProgressPercentage))
+		}
+		return v, getTaskStat(v.Status), nil
+	}
+}
+
+func getTaskStat(pr *import1.TaskStatus) string {
+	const two, three, five, six, seven = 2, 3, 5, 6, 7
+	if pr != nil {
+		if *pr == import1.TaskStatus(six) {
+			return "FAILED"
+		}
+		if *pr == import1.TaskStatus(seven) {
+			return "CANCELED"
+		}
+		if *pr == import1.TaskStatus(two) {
+			return "QUEUED"
+		}
+		if *pr == import1.TaskStatus(three) {
+			return "RUNNING"
+		}
+		if *pr == import1.TaskStatus(five) {
+			return "SUCCEEDED"
+		}
+	}
+	return "UNKNOWN"
 }
