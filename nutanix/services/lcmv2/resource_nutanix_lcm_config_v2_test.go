856a68946c99ca26c504308ec5532a9eb22f3c44
Gullapalli Akhil Sai <akhilsai.gullapalli@nutanix.com>
2025-02-24 16:40:27 +0530

    Rename files to follow a specifc format and add testcase for LCM config

---
+++ nutanix/services/lcmv2/resource_nutanix_lcm_config_v2_test.go
@@ -0,0 +1,65 @@
+package lcmv2_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	acc "github.com/terraform-providers/terraform-provider-nutanix/nutanix/acctest"
+)
+
+func TestAccV2NutanixLcmConfigUpdate(t *testing.T) {
+	datasourceLcmConfigBeforeUpdate := "data.nutanix_lcm_config_v2.get-lcm-config-before-update"
+	resourceLcmConfig := "nutanix_lcm_config_v2.update-lcm-config"
+	datasourceLcmConfigAfterUpdate := "data.nutanix_lcm_config_v2.get-lcm-config-after-update"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() {},
+		Providers: acc.TestAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testLcmUpdateConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					// Check Auto Inventory is disabled Before Update
+					resource.TestCheckResourceAttr(datasourceLcmConfigBeforeUpdate, "is_auto_inventory_enabled", "false"),
+
+					// Update LCM Config Check
+					resource.TestCheckResourceAttr(resourceLcmConfig, "is_auto_inventory_enabled", "true"),
+
+					// Check Auto Inventory is enabled after Update
+					resource.TestCheckResourceAttr(datasourceLcmConfigAfterUpdate, "is_auto_inventory_enabled", "true"),
+				),
+			},
+		},
+	})
+}
+
+func testLcmUpdateConfig() string {
+	return `
+# Get LCM Config before Update
+data "nutanix_lcm_config_v2" "get-lcm-config-before-update" {
+	lifecycle {
+		postcondition {
+			condition     = self.is_auto_inventory_enabled == false
+			error_message = "Auto Inventory is already enabled, current value: ${self.is_auto_inventory_enabled}"
+	   }
+  	}
+}
+
+# Update LCM Config: Enable auto inventory
+resource "nutanix_lcm_config_v2" "update-lcm-config" {
+	is_auto_inventory_enabled = true
+	depends_on = [data.nutanix_lcm_config_v2.get-lcm-config-before-update]
+}
+
+# Get LCM Config after Update
+data "nutanix_lcm_config_v2" "get-lcm-config-after-update" {
+   lifecycle {
+		postcondition {
+			condition     = self.is_auto_inventory_enabled == true
+			error_message = "Auto Inventory is not enabled, current value: ${self.is_auto_inventory_enabled}"
+	   }
+  	}
+	depends_on = [nutanix_lcm_config_v2.update-lcm-config]
+}
+`
+}
