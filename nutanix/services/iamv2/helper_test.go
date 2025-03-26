a981b95446662fd947150bd39475910b174e087c
Haroon Dweikat Ntx <haroon.dweikat@nutanix.com>
2025-03-26 10:10:02 +0200

    pre requests for saml idp test cases  (#788)

--- nutanix/services/iamv2/helper_test.go
+++ nutanix/services/iamv2/helper_test.go
@@ -68,12 +68,15 @@ func testAccCheckNutanixDirectoryServicesV2Destroy(s *terraform.State) error {
 	conn := acc.TestAccProvider.Meta().(*conns.Client)
 
 	for _, rs := range s.RootModule().Resources {
-		if rs.Type != "nutanix_volume_group_v2" {
+		if rs.Type != "nutanix_directory_services_v2" {
 			continue
 		}
-
+		fmt.Printf("Checking directory service : %s", rs.Primary.ID)
 		readResp, errRead := conn.IamAPI.DirectoryServiceAPIInstance.GetDirectoryServiceById(utils.StringPtr(rs.Primary.ID))
 		if errRead != nil {
+			if strings.Contains(fmt.Sprint(errRead), "Directory service not found") {
+				return nil
+			}
 			return errRead
 		}
 		// get etag value from read response to pass in update request If-Match header, Required for update request
@@ -81,8 +84,41 @@ func testAccCheckNutanixDirectoryServicesV2Destroy(s *terraform.State) error {
 		headers := make(map[string]interface{})
 		headers["If-Match"] = utils.StringPtr(etagValue)
 
+		fmt.Println("Deleting directory service")
+
 		if _, err := conn.IamAPI.DirectoryServiceAPIInstance.DeleteDirectoryServiceById(utils.StringPtr(rs.Primary.ID), headers); err != nil {
 			if strings.Contains(fmt.Sprint(err), "Directory service not found") {
+				return nil
+			}
+			return err
+		}
+	}
+	return nil
+}
+
+func testAccCheckNutanixUserGroupsV2Destroy(s *terraform.State) error {
+	conn := acc.TestAccProvider.Meta().(*conns.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "nutanix_user_groups_v2" {
+			continue
+		}
+
+		readResp, errRead := conn.IamAPI.UserGroupsAPIInstance.GetUserGroupById(utils.StringPtr(rs.Primary.ID))
+		if errRead != nil {
+			if strings.Contains(fmt.Sprint(errRead), "the requested user group does not exist") {
+				return nil
+			}
+			return errRead
+		}
+
+		// get etag value from read response to pass in update request If-Match header, Required for update request
+		etagValue := conn.IamAPI.DirectoryServiceAPIInstance.ApiClient.GetEtag(readResp)
+		headers := make(map[string]interface{})
+		headers["If-Match"] = utils.StringPtr(etagValue)
+
+		if _, err := conn.IamAPI.UserGroupsAPIInstance.DeleteUserGroupById(utils.StringPtr(rs.Primary.ID), headers); err != nil {
+			if strings.Contains(fmt.Sprint(err), "the requested user group does not exist") {
 				return nil
 			}
 			return err
