ed31e3d4593d49c3e411f2036c01c2262679ab71
Haroon-Dweikat-Ntx <haroon.dweikat@nutanix.com>
2025-04-08 20:10:30 +0200

    set IsNatEnabled to false in api payload if set to false in Tf script

--- nutanix/common/common.go
+++ nutanix/common/common.go
@@ -1,5 +1,11 @@
 package common
 
+import (
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
 // ExpandListOfString to expand a list of interface{}
 // which is defined in the schema
 // its return a list of string
@@ -9,4 +15,21 @@ func ExpandListOfString(list []interface{}) []string {
 		stringListStr[i] = v.(string)
 	}
 	return stringListStr
+}
+
+// defined to determine whether a particular key (or configuration attribute) within a Terraform resource configuration has been explicitly set by the user.
+// Returns a Boolean (true or false). true indicates that the key was explicitly set with a non-null value; false implies it was either not set, is unknown, or explicitly set to null.
+func IsExplicitlySet(d *schema.ResourceData, key string) bool {
+	rawConfig := d.GetRawConfig() // Get raw Terraform config as cty.Value
+	if rawConfig.IsNull() || !rawConfig.IsKnown() {
+		return false // If rawConfig is null/unknown, key wasn't explicitly set
+	}
+
+	// Convert rawConfig to map and check if key exists
+	configMap := rawConfig.AsValueMap()
+	if val, exists := configMap[key]; exists {
+		log.Printf("[DEBUG] Key: %s, Value: %s", key, val)
+		return !val.IsNull() // Ensure key exists and isn't explicitly null
+	}
+	return false
 }
