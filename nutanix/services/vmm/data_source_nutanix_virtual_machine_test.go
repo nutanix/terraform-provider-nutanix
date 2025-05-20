2bff2068e34c597bbcbeaa426865f14e8749d8bb
Gullapalli Akhil Sai <akhilsai.gullapalli@nutanix.com>
2025-05-20 18:21:01 +0530

    Issue: 649: Add TestCase

--- nutanix/services/vmm/data_source_nutanix_virtual_machine_test.go
+++ nutanix/services/vmm/data_source_nutanix_virtual_machine_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"testing"
+	"regexp"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -74,6 +75,23 @@ func TestAccNutanixVirtualMachineDataSource_withDiskContainer(t *testing.T) {
 		},
 	})
 }
+
+func TestAccNutanixVirtualMachineNegativeScenario(t *testing.T) {
+	// This test is to check the requested image size less than the disk size
+	// and it should return an error which internally also tests issues/649
+	r := acctest.RandInt()
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() {},
+		Providers: acc.TestAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVMRequestedImageSizeLess(r),
+				ExpectError: regexp.MustCompile("Requested image size 1048576 is less than actual size 41126400"),
+			},
+		},
+	})
+}
+
 
 func testAccVMDataSourceWithDiskContainer(vmName, containerUUID string) string {
 	return fmt.Sprintf(`
@@ -193,5 +211,47 @@ func testAccVMDataSourceConfigWithDisk(r int) string {
 		vm_id = "${nutanix_virtual_machine.vm1.id}"
 	}
 
+	`, r)
+}
+
+func testAccVMRequestedImageSizeLess(r int) string {
+	// This test is to check the requested image size less than the disk size
+	return fmt.Sprintf(`
+	data "nutanix_clusters" "clusters" {}
+
+	locals {
+			cluster_ext_id = "${data.nutanix_clusters.clusters.entities.0.service_list.0 == "PRISM_CENTRAL"
+			? data.nutanix_clusters.clusters.entities.1.metadata.uuid : data.nutanix_clusters.clusters.entities.0.metadata.uuid}"
+	}
+
+	resource "nutanix_image" "cirros-034-disk" {
+				name = "cirros-034-disk-%[1]d"
+				source_uri  = "http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img"
+				description = "heres a tiny linux image, not an iso, but a real disk!"
+	}
+
+	resource "nutanix_virtual_machine" "vm1" {
+	name = "test-example-%[1]d"
+	cluster_uuid= data.nutanix_clusters.clusters.entities.0.metadata.uuid
+	num_vcpus_per_socket = 2
+	num_sockets     = 2
+	memory_size_mib   = 1000
+	disk_list {
+			data_source_reference = {
+				kind = "image"
+				uuid = nutanix_image.cirros-034-disk.id
+			}
+			device_properties {
+				disk_address = {
+					device_index = 0
+					adapter_type = "SCSI"
+				}
+
+				device_type = "DISK"
+			}
+			disk_size_mib   = 1
+			disk_size_bytes = 1
+	}
+	}
 	`, r)
 }
