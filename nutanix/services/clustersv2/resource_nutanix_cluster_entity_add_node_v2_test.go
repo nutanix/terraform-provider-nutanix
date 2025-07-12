7cf333f4239b6cbe41678a9eaabd2d79abcd5aba
Haroon-Dweikat-Ntx <haroon.dweikat@nutanix.com>
2025-07-13 00:05:21 +0300

    fix ssh reset password command

--- nutanix/services/clustersv2/resource_nutanix_cluster_entity_add_node_v2_test.go
+++ nutanix/services/clustersv2/resource_nutanix_cluster_entity_add_node_v2_test.go
@@ -158,7 +158,7 @@ resource "nutanix_cluster_v2" "cluster-3nodes" {
   }
 
   provisioner "local-exec" {
-    command = "ssh-keygen -f '~/.ssh/known_hosts' -R '${local.clusters.nodes[0].cvm_ip}';  sshpass -p '${local.clusters.pe_password}' ssh -o StrictHostKeyChecking=no ${local.clusters.pe_username}@${local.clusters.nodes[1].cvm_ip} '/home/nutanix/prism/cli/ncli user reset-password user-name=${local.clusters.nodes[1].username} password=${local.clusters.nodes[1].password}'"
+    command = "ssh-keygen -f ~/.ssh/known_hosts -R ${local.clusters.nodes[1].cvm_ip};   sshpass -p '${local.clusters.pe_password}' ssh -o StrictHostKeyChecking=no ${local.clusters.pe_username}@${local.clusters.nodes[1].cvm_ip} '/home/nutanix/prism/cli/ncli user reset-password user-name=${local.clusters.nodes[1].username} password=${local.clusters.nodes[1].password}'"
 
     on_failure = continue
   }
