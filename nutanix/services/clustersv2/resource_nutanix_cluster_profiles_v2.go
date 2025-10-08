92f9ad659b64bd745b87a0f6cc6117c96eb0337d
Haroon-Dweikat-Ntx <haroon.dweikat@nutanix.com>
2025-10-08 08:50:27 +0300

    add cluster profile to cluster client

---
+++ nutanix/services/clustersv2/resource_nutanix_cluster_profiles_v2.go
@@ -0,0 +1,91 @@
+package clustersv2
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
+
+func ResourceNutanixClusterProfilesV2() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: ResourceNutanixClusterProfilesV2Create,
+		ReadContext:   ResourceNutanixClusterProfilesV2Read,
+		UpdateContext: ResourceNutanixClusterProfilesV2Update,
+		DeleteContext: ResourceNutanixClusterProfilesV2Delete,
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"allowed_overrides": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type:         schema.TypeString,
+					ValidateFunc: validation.StringInSlice(allowedConfigTypeNames, false),
+				},
+			},
+			"name_server_ip_list": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"ipv4": SchemaForValuePrefixLength(ipv4PrefixLengthDefaultValue),
+						"ipv6": SchemaForValuePrefixLength(ipv6PrefixLengthDefaultValue),
+					},
+				},
+			},
+
+			// Computed fields
+			"ext_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"tenant_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"links": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"href": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"rel": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func ResourceNutanixClusterProfilesV2Create(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	return ResourceNutanixClusterProfilesV2Read(ctx, d, meta)
+}
+
+func ResourceNutanixClusterProfilesV2Read(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	return nil
+}
+
+func ResourceNutanixClusterProfilesV2Update(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	return ResourceNutanixClusterProfilesV2Read(ctx, d, meta)
+}
+
+func ResourceNutanixClusterProfilesV2Delete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	return nil
+}
