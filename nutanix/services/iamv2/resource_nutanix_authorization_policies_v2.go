58fcf18d0d1b78377a194acc142ab488222e55fc
Haroon Dweikat Ntx <haroon.dweikat@nutanix.com>
2025-11-18 17:40:05 +0200

    🔧 Update Go version to 1.25.3 (latest stable) (#973)
    Co-authored-by: Gullapalli Akhil Sai <akhilsai.gullapalli@nutanix.com>
    Co-authored-by: Jason Cantrell <jason.cantrell@nutanix.com>

--- nutanix/services/iamv2/resource_nutanix_authorization_policies_v2.go
+++ nutanix/services/iamv2/resource_nutanix_authorization_policies_v2.go
@@ -358,7 +358,7 @@ func expandIdentityFilter(identities []interface{}) ([]import1.IdentityFilter, e
 				log.Printf("[DEBUG] expandIdentityFilter val type : %v\n", reflect.TypeOf(val))
 				reserved, err := deserializeJSONStringToMap(val.(string))
 				if err != nil {
-					return nil, fmt.Errorf(err.Error())
+					return nil, fmt.Errorf("%s", err.Error())
 				}
 				log.Printf("[DEBUG] expandIdentityFilter reserved : %v\n", reserved)
 				filter.Reserved_ = reserved
@@ -395,7 +395,7 @@ func expandEntityFilter(entities []interface{}) ([]import1.EntityFilter, error) 
 				log.Printf("[DEBUG] expandEntityFilter val type : %v\n", reflect.TypeOf(val))
 				reserved, err := deserializeJSONStringToMap(val.(string))
 				if err != nil {
-					return nil, fmt.Errorf(err.Error())
+					return nil, fmt.Errorf("%s", err.Error())
 				}
 				log.Printf("[DEBUG] expandEntityFilter reserved : %v\n", reserved)
 				filter.Reserved_ = reserved
