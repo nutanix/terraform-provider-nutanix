c9db5cedebb249fff8e845e8abe94be78efb3e4f
andrew-sumner <andrew-sumner@users.noreply.github.com>
2026-04-01 01:26:39 +1300

    [Feat] API Key and custom headers as an alternative method of authorisation (#1062)
    Co-authored-by: Andrew Sumner <andrew.sumner@datatorque.com>
    Co-authored-by: Gullapalli Akhil Sai <akhilsai.gullapalli@nutanix.com>

--- nutanix/sdks/v4/networking/networking.go
+++ nutanix/sdks/v4/networking/networking.go
@@ -1,8 +1,6 @@
 package networking
 
 import (
-	"strconv"
-
 	"github.com/nutanix/ntnx-api-golang-clients/networking-go-client/v4/api"
 	network "github.com/nutanix/ntnx-api-golang-clients/networking-go-client/v4/client"
 	"github.com/terraform-providers/terraform-provider-nutanix/nutanix/client"
@@ -22,32 +20,23 @@ type Client struct {
 func NewNetworkingClient(credentials client.Credentials) (*Client, error) {
 	var baseClient *network.ApiClient
 
-	// check if all required fields are present. Else create an empty client
-	if credentials.Username != "" && credentials.Password != "" && credentials.Endpoint != "" {
-		pcClient := network.NewApiClient()
-
-		pcClient.Host = credentials.Endpoint
-		pcClient.Password = credentials.Password
-		pcClient.Username = credentials.Username
-		pcClient.Port = sdkconfig.DefaultPort
-		if credentials.Port != "" {
-			if p, err := strconv.Atoi(credentials.Port); err == nil {
-				pcClient.Port = p
-			}
-		}
-		pcClient.VerifySSL = false
-		pcClient.AllowVersionNegotiation = sdkconfig.AllowVersionNegotiation
+	pcClient := network.NewApiClient()
+	if cfg := sdkconfig.ConfigureV4Client(credentials, pcClient); cfg != nil {
+		pcClient.Host = cfg.Host
+		pcClient.Port = cfg.Port
+		pcClient.Username = cfg.Username
+		pcClient.Password = cfg.Password
+		pcClient.VerifySSL = cfg.VerifySSL
+		pcClient.AllowVersionNegotiation = cfg.AllowVersionNegotiation
 		baseClient = pcClient
 	}
 
-	f := &Client{
+	return &Client{
 		Routes:                api.NewRoutesApi(baseClient),
 		RoutesTable:           api.NewRouteTablesApi(baseClient),
 		RoutingPolicy:         api.NewRoutingPoliciesApi(baseClient),
 		SubnetAPIInstance:     api.NewSubnetsApi(baseClient),
 		VpcAPIInstance:        api.NewVpcsApi(baseClient),
 		FloatingIPAPIInstance: api.NewFloatingIpsApi(baseClient),
-	}
-
-	return f, nil
+	}, nil
 }
--- nutanix/sdks/v4/volumes/volumes.go
+++ nutanix/sdks/v4/volumes/volumes.go
@@ -1,10 +1,8 @@
 package volumes
 
 import (
-	"strconv"
-
 	"github.com/nutanix/ntnx-api-golang-clients/volumes-go-client/v4/api"
-	prism "github.com/nutanix/ntnx-api-golang-clients/volumes-go-client/v4/client"
+	volumes "github.com/nutanix/ntnx-api-golang-clients/volumes-go-client/v4/client"
 	"github.com/terraform-providers/terraform-provider-nutanix/nutanix/client"
 	"github.com/terraform-providers/terraform-provider-nutanix/nutanix/sdks/v4/sdkconfig"
 )
@@ -15,30 +13,21 @@ type Client struct {
 }
 
 func NewVolumeClient(credentials client.Credentials) (*Client, error) {
-	var baseClient *prism.ApiClient
+	var baseClient *volumes.ApiClient
 
-	// check if all required fields are present. Else create an empty client
-	if credentials.Username != "" && credentials.Password != "" && credentials.Endpoint != "" {
-		pcClient := prism.NewApiClient()
-
-		pcClient.Host = credentials.Endpoint
-		pcClient.Password = credentials.Password
-		pcClient.Username = credentials.Username
-		pcClient.Port = sdkconfig.DefaultPort
-		if credentials.Port != "" {
-			if p, err := strconv.Atoi(credentials.Port); err == nil {
-				pcClient.Port = p
-			}
-		}
-		pcClient.VerifySSL = false
-		pcClient.AllowVersionNegotiation = sdkconfig.AllowVersionNegotiation
+	pcClient := volumes.NewApiClient()
+	if cfg := sdkconfig.ConfigureV4Client(credentials, pcClient); cfg != nil {
+		pcClient.Host = cfg.Host
+		pcClient.Port = cfg.Port
+		pcClient.Username = cfg.Username
+		pcClient.Password = cfg.Password
+		pcClient.VerifySSL = cfg.VerifySSL
+		pcClient.AllowVersionNegotiation = cfg.AllowVersionNegotiation
 		baseClient = pcClient
 	}
 
-	f := &Client{
+	return &Client{
 		VolumeAPIInstance:      api.NewVolumeGroupsApi(baseClient),
 		IscsiClientAPIInstance: api.NewIscsiClientsApi(baseClient),
-	}
-
-	return f, nil
+	}, nil
 }
