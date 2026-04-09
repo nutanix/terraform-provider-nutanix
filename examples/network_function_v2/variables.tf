0576dd663dee450042096af80668cbc1dc9e3535
Haroon Dweikat Ntx <haroon.dweikat@nutanix.com>
2026-04-09 12:07:57 +0300

    Enhance network_function_v2 example with complete VM and subnet deployment

--- examples/network_function_v2/variables.tf
+++ examples/network_function_v2/variables.tf
@@ -3,7 +3,8 @@ variable "nutanix_username" {
 }
 
 variable "nutanix_password" {
-  type = string
+  type      = string
+  sensitive = true
 }
 
 variable "nutanix_endpoint" {
@@ -11,21 +12,80 @@ variable "nutanix_endpoint" {
 }
 
 variable "nutanix_port" {
-  type    = string
-  default = "9440"
+  type    = number
+  default = 9440
 }
 
-variable "network_function_name" {
-  type    = string
-  default = "tf-network-function"
-}
-
-variable "ingress_nic_reference" {
-  type = string
-}
-
-variable "egress_nic_reference" {
+variable "cluster_name" {
   type    = string
   default = ""
 }
 
+variable "image_name" {
+  type = string
+}
+
+variable "network_function_name" {
+  type    = string
+  default = "tf-network-function-inline"
+}
+
+variable "network_function_description" {
+  type    = string
+  default = "Inline network function managed by Terraform"
+}
+
+variable "management_subnet_name" {
+  type    = string
+  default = "tf-network-function-mgmt"
+}
+
+variable "management_subnet_vlan_id" {
+  type = number
+}
+
+variable "management_subnet_network" {
+  type = string
+}
+
+variable "management_subnet_prefix_length" {
+  type    = number
+  default = 24
+}
+
+variable "management_subnet_gateway" {
+  type = string
+}
+
+variable "management_subnet_pool_start" {
+  type = string
+}
+
+variable "management_subnet_pool_end" {
+  type = string
+}
+
+variable "nf_vm_admin_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "vm_num_sockets" {
+  type    = number
+  default = 2
+}
+
+variable "vm_num_cores_per_socket" {
+  type    = number
+  default = 2
+}
+
+variable "vm_memory_size_bytes" {
+  type    = number
+  default = 4294967296
+}
+
+variable "vm_disk_size_bytes" {
+  type    = number
+  default = 21474836480
+}
