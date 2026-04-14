6dada59c73ac2d8995817aeb87ab4fd438cbaf66
Haroon Dweikat Ntx <haroon.dweikat@nutanix.com>
2026-04-14 16:28:08 +0300

    Document PE password reset workflow and credential types in clusters_v2 example

--- examples/clusters_v2/variables.tf
+++ examples/clusters_v2/variables.tf
@@ -1,4 +1,8 @@
 #define the type of variables to be used in terraform file
+
+# Provider Credentials (nutanix_username / nutanix_password): 
+# These are used by the Terraform provider to authenticate 
+# with your Prism Central (PC) to execute the plan.
 variable "nutanix_username" {
   type = string
 }
@@ -11,6 +15,8 @@ variable "nutanix_endpoint" {
 variable "nutanix_port" {
   type = string
 }
+# PE Credentials (pe_username / pe_password):
+# These are the credentials of the PE to be used for the ssh to the node.
 variable "pe_username" {
   type = string
 }
@@ -22,6 +28,9 @@ variable "node_ip" {
   type = string
 }
 
+# Registration Credentials (username / password): 
+# These are the new credentials you want to set for the PE. 
+# The local-exec command sets these on the PE, and the nutanix_pc_registration_v2 resource then uses them to complete the registration. 
 variable "username" {
   type = string
 }
