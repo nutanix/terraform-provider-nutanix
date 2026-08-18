64c46cb27bdbe35b11973e398dfaadfc931e0452
Gullapalli Akhil Sai <akhilsai.gullapalli@nutanix.com>
2026-08-18 15:22:55 +0530

    fix lints

--- nutanix/services/vmmv2/resource_nutanix_ngt_insert_iso_v2.go
+++ nutanix/services/vmmv2/resource_nutanix_ngt_insert_iso_v2.go
@@ -300,7 +300,7 @@ func ResourceNutanixNGTInsertIsoV2Delete(ctx context.Context, d *schema.Resource
 	log.Printf("[DEBUG] ResourceNutanixNGTInsertIsoV2Delete : Ejecting NGT ISO from the CD-ROM %s of the VM %s", d.Get("cdrom_ext_id").(string), d.Get("vm_ext_id").(string))
 
 	cdromExtID, cdromExists := d.GetOk("cdrom_ext_id")
-	if action, ok := d.GetOk("action"); ok && action.(string) == "eject" || !cdromExists || cdromExtID.(string) == ""  {
+	if action, ok := d.GetOk("action"); ok && action.(string) == "eject" || !cdromExists || cdromExtID.(string) == "" {
 		return diag.Diagnostics{{
 			Severity: diag.Warning,
 			Summary:  "NGT ISO is not inserted on the CD-ROM of the VM or ejected earlier using an action, Ignoring the request to eject the NGT ISO",
