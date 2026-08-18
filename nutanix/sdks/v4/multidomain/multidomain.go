3f50acd358bdccbab23af5b31a6874ea5fcc6241
Haroon Dweikat Ntx <haroon.dweikat@nutanix.com>
2026-08-18 09:55:13 +0300

    fix multidomain clinet

--- nutanix/sdks/v4/multidomain/multidomain.go
+++ nutanix/sdks/v4/multidomain/multidomain.go
@@ -16,26 +16,27 @@ type Client struct {
 func NewMultidomainClient(credentials client.Credentials) (*Client, error) {
 	var baseClient *multidomainClient.ApiClient
 
-	// check if all required fields are present. Else create an empty client
-	if credentials.Username != "" && credentials.Password != "" && credentials.Endpoint != "" {
-		pcClient := multidomainClient.NewApiClient()
-
-		if cfg := sdkconfig.ConfigureV4Client(credentials, pcClient); cfg != nil {
-			pcClient.Host = cfg.Host
-			pcClient.Port = cfg.Port
-			pcClient.Username = cfg.Username
-			pcClient.Password = cfg.Password
-			pcClient.VerifySSL = cfg.VerifySSL
-			pcClient.AllowVersionNegotiation = cfg.AllowVersionNegotiation
-		}
+	pcClient := multidomainClient.NewApiClient()
+	if cfg := sdkconfig.ConfigureV4Client(credentials, pcClient); cfg != nil {
+		pcClient.Host = cfg.Host
+		pcClient.Port = cfg.Port
+		pcClient.Username = cfg.Username
+		pcClient.Password = cfg.Password
+		pcClient.VerifySSL = cfg.VerifySSL
+		pcClient.AllowVersionNegotiation = cfg.AllowVersionNegotiation
 		baseClient = pcClient
 	}
+	return &Client{
+		DirectoryServiceAPIInstance: api.NewDirectoryServicesServiceApi(baseClient),
+		SamlIdentityAPIInstance:     api.NewSAMLIdentityProvidersServiceApi(baseClient),
+		UserGroupsAPIInstance:       api.NewUserGroupsServiceApi(baseClient),
+		RolesAPIInstance:            api.NewRolesServiceApi(baseClient),
+		OperationsAPIInstance:       api.NewOperationsServiceApi(baseClient),
+		UsersAPIInstance:            api.NewUsersServiceApi(baseClient),
+		AuthAPIInstance:             api.NewAuthorizationPoliciesServiceApi(baseClient),
+		RoleMembershipAPIInstance:   api.NewRoleMembershipServiceApi(baseClient),
+		EntityAPIInstance:           api.NewEntitiesServiceApi(baseClient),
+		APIClientInstance:           baseClient,
+	}, nil
 
-	f := &Client{
-		Projects:          api.NewProjectsServiceApi(baseClient),
-		ResourceGroups:    api.NewResourceGroupsServiceApi(baseClient),
-		APIClientInstance: baseClient,
-	}
-
-	return f, nil
 }
