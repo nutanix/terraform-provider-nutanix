80ea3a7f59947280048380afa0b21ffc54820a74
Haroon Dweikat Ntx <haroon.dweikat@nutanix.com>
2026-09-05 00:11:39 +0300

    lint fix

--- nutanix/services/lcmv2/resource_nutanix_lcm_upgrade_v2.go
+++ nutanix/services/lcmv2/resource_nutanix_lcm_upgrade_v2.go
@@ -139,7 +139,7 @@ func ResourceLcmUpgradeV2Create(ctx context.Context, d *schema.ResourceData, met
 
 	aJSON, _ = json.MarshalIndent(performUpgradeRequest, "", "  ")
 	log.Printf("[DEBUG] Perform Upgrade Request: %s", string(aJSON))
-	
+
 	resp, err := conn.LcmUpgradeAPIInstance.PerformUpgrade(ctx, &performUpgradeRequest)
 	if err != nil {
 		return diag.Errorf("error while Perform Upgrade the LCM config: %v", err)
